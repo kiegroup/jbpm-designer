d65daac3c786acd90b470d613000a2783fc57e4f
martin.czuchra <martin.czuchra@d672c736-503d-0410-a38a-9366997c882b>
2007-12-05 15:58:48 +0000

    Adding license information. The theme is now grey, as this is an unpatched theme of ext. Fixing issue 34, and a minor issue with error/image display.
    git-svn-id: http://oryx-editor.googlecode.com/svn/trunk@30 d672c736-503d-0410-a38a-9366997c882b

--- server.php
+++ server.php
@@ -1,6 +1,31 @@
 <?
+/**
+ * Copyright (c) 2006
+ * Martin Czuchra, Nicolas Peters, Daniel Polak, Willi Tscheschner
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ **/
 
 /* This is an alternative back end for the Oryx Business Process Editor */
+
+// no error reporting in production mode.
+error_reporting(0);
 
 // constants for error display
 define('DATABASE_DOWN', 'The database seems to be down. Try again later!');
@@ -137,7 +162,8 @@ function writeToMySQL($resource, $data) {
 
 }
 
-function template($body){
+function template($body) {
+	global $resource;
 	return '<?xml version="1.0" encoding="utf-8"?>
 	<html xmlns="http://www.w3.org/1999/xhtml"
 					    xmlns:b3mn="http://b3mn.org/2007/b3mn"
@@ -158,7 +184,7 @@ function template($body){
         <script src="lib/ext-1.0/ColorField.js" type="text/javascript" />
         <style media="screen" type="text/css">
 			@import url("lib/ext-1.0/resources/css/ext-all.css");
-			@import url("lib/ext-1.0/resources/css/ytheme-blue.css");
+			@import url("lib/ext-1.0/resources/css/ytheme-grey.css");
 		</style>
 
 				
@@ -203,7 +229,7 @@ function showError($nice, $real) {
 </head>
 <body>
 	<div style='text-align: center;'>
-		<img src='./src/images/crystal/error.png' style='padding-top: 200px;' width='128' height='128'/>
+		<img src='./images/crystal/error.png' style='padding-top: 200px;' width='128' height='128'/>
 		<p><? echo $nice ?></p>
 		<!-- ACTUAL ERROR: <? echo $real ?> -->
 	</div>
