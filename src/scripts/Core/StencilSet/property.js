21bef599082aeb3db473017cbe6284c8cf0d9ca7
martin.czuchra <martin.czuchra@d672c736-503d-0410-a38a-9366997c882b>
2007-12-05 17:10:40 +0000

    Some improved defaults handling for properties. Type now defaults to String, if not set.
    git-svn-id: http://oryx-editor.googlecode.com/svn/trunk@32 d672c736-503d-0410-a38a-9366997c882b

--- src/scripts/Core/StencilSet/property.js
+++ src/scripts/Core/StencilSet/property.js
@@ -1,3 +1,4 @@
+
 /**
  * Copyright (c) 2006
  * Martin Czuchra, Nicolas Peters, Daniel Polak, Willi Tscheschner
@@ -20,13 +21,18 @@
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  * DEALINGS IN THE SOFTWARE.
  **/
-
 /**
  * Init namespace
  */
-if(!ORYX) {var ORYX = {};}
-if(!ORYX.Core) {ORYX.Core = {};}
-if(!ORYX.Core.StencilSet) {ORYX.Core.StencilSet = {};}
+if (!ORYX) {
+    var ORYX = {};
+}
+if (!ORYX.Core) {
+    ORYX.Core = {};
+}
+if (!ORYX.Core.StencilSet) {
+    ORYX.Core.StencilSet = {};
+}
 
 /**
  * Class Property
@@ -35,311 +41,289 @@ if(!ORYX.Core.StencilSet) {ORYX.Core.StencilSet = {};}
  */
 ORYX.Core.StencilSet.Property = Clazz.extend({
 
-	/**
-	 * Constructor
-	 */
-	construct: function(jsonProp, namespace, stencil) {
-		arguments.callee.$.construct.apply(this, arguments);
-
-		if(!jsonProp) {
-			throw "ORYX.Core.StencilSet.Property(construct): Parameter jsonProp is not defined.";
-		}
-		if(!namespace) {
-			throw "ORYX.Core.StencilSet.Property(construct): Parameter namespace is not defined.";
-		}
-		if(!stencil) {
-			throw "ORYX.Core.StencilSet.Property(construct): Parameter stencil is not defined.";
-		}
-		
-		this._jsonProp = jsonProp;
-		this._namespace = namespace;
-		this._stencil = stencil;
-
-		this._items = new Hash();
-		this._complexItems = new Hash();
-
-		//init all values
-		if(!jsonProp.id) {
-			throw "ORYX.Core.StencilSet.Property(construct): Id is not defined.";
-		}
-		
-		if(!jsonProp.type) {
-			throw "ORYX.Core.StencilSet.Property(construct): Type is not defined.";
-		} else {
-			jsonProp.type = jsonProp.type.toLowerCase();
-		}
-		
-		if(!jsonProp.prefix) {
-			jsonProp.prefix = "oryx";
-		}
-		
-		if(!jsonProp.title) {
-			jsonProp.title = "";
-		}
-		
-		if(!jsonProp.value) {
-			jsonProp.value = "";
-		}
-		
-		if(!jsonProp.description) {
-			jsonProp.description = "";
-		}
-		
-		if(!jsonProp.readonly) {
-			jsonProp.readonly = false;
-		}
-		
-		if(!jsonProp.optional) {
-			jsonProp.optional = true;
-		}
-		
-		//init refToView
-		if(this._jsonProp.refToView) {
-			if(!(this._jsonProp.refToView instanceof Array)) {
-				this._jsonProp.refToView = [this._jsonProp.refToView];
-			}
-		} else {
-			this._jsonProp.refToView = [];
-		}
-
-		if(jsonProp.min === undefined || jsonProp.min === null) {
-			jsonProp.min = Number.MIN_VALUE;
-		}
-		
-		if(jsonProp.max === undefined || jsonProp.max === null) {
-			jsonProp.max = Number.MAX_VALUE;
-		}
-		
-		if(!jsonProp.fillOpacity) {
-			jsonProp.fillOpacity = false; 
-		}
-		
-		if(!jsonProp.strokeOpacity) {
-			jsonProp.strokeOpacity = false; 
-		}
-		
-		if(jsonProp.length === undefined || jsonProp.length === null) {
-			jsonProp.length = Number.MAX_VALUE;
-		}
-		
-		if(!jsonProp.wrapLines) {
-			jsonProp.wrapLines = false; 
-		}
-		
-		if(!jsonProp.dateFormat) {
-			jsonProp.dataFormat = "m/d/y";
-		}
-		
-		if(!jsonProp.fill) {
-			jsonProp.fill = false;
-		}
-		
-		if(!jsonProp.stroke) {
-			jsonProp.stroke = false;
-		}
-		
-		if(jsonProp.type === ORYX.CONFIG.TYPE_CHOICE) {
-			if(jsonProp.items && jsonProp.items instanceof Array) {
-				jsonProp.items.each((function(jsonItem) {
-					this._items[jsonItem.value] = new ORYX.Core.StencilSet.PropertyItem(jsonItem, namespace, this);
-				}).bind(this));
-			} else {
-				throw "ORYX.Core.StencilSet.Property(construct): No property items defined."
-			}
-		// extended by Kerstin (start)
-		} else if(jsonProp.type === ORYX.CONFIG.TYPE_COMPLEX) {
-			if(jsonProp.complexItems && jsonProp.complexItems instanceof Array) {
-				jsonProp.complexItems.each((function(jsonComplexItem) {
-					this._complexItems[jsonComplexItem.id] = new ORYX.Core.StencilSet.ComplexPropertyItem(jsonComplexItem, namespace, this);
-				}).bind(this));
-			} else {
-				throw "ORYX.Core.StencilSet.Property(construct): No complex property items defined."
-			}
-		}
-		// extended by Kerstin (end)
-	},
-
-	/**
-	 * @param {ORYX.Core.StencilSet.Property} property
-	 * @return {Boolean} True, if property has the same namespace and id.
-	 */
-	equals: function(property) {
-		return (this._namespace === property.namespace() &&
-			this.id() === property.id()) ? true : false;
-	},
-
-	namespace: function() {
-		return this._namespace;
-	},
-
-	stencil: function() {
-		return this._stencil;
-	},
-
-	id: function() {
-		return this._jsonProp.id;
-	},
-	
-	prefix: function() {
-		return this._jsonProp.prefix;
-	},
-
-	type: function() {
-		return this._jsonProp.type;
-	},
-
-	title: function() {
-		return this._jsonProp.title;
-	},
-
-	value: function() {
-		return this._jsonProp.value;
-	},
-	
-	readonly: function() {
-		return this._jsonProp.readonly;
-	},
-
-	optional: function() {
-		return this._jsonProp.optional;
-	},
-
-	description: function() {
-		return this._jsonProp.description;
-	},
-
-	/**
-	 * An optional link to a SVG element so that the property affects the
-	 * graphical representation of the stencil.
-	 */
-	refToView: function() {
-		return this._jsonProp.refToView;
-	},
-
-	/**
-	 * If type is integer or float, min is the lower bounds of value.
-	 */
-	min: function() {
-		return this._jsonProp.min;
-	},
-
-	/**
-	 * If type ist integer or float, max is the upper bounds of value.
-	 */
-	max: function() {
-		return this._jsonProp.max;
-	},
-	
-	/**
-	 * If type is float, this method returns if the fill-opacity property should
-	 *  be set.
-	 *  @return {Boolean}
-	 */
-	fillOpacity: function() {
-		return this._jsonProp.fillOpacity;
-	},
-	
-	/**
-	 * If type is float, this method returns if the stroke-opacity property should
-	 *  be set.
-	 *  @return {Boolean}
-	 */
-	strokeOpacity: function() {
-		return this._jsonProp.strokeOpacity;
-	},
-
-	/**
-	 * If type is string or richtext, length is the maximum length of the text.
-	 * TODO how long can a string be.
-	 */
-	length: function() {
-		return this._jsonProp.length ?  this._jsonProp.length : Number.MAX_VALUE;
-	},
-	
-	wrapLines: function() {
-		return this._jsonProp.wrapLines;
-	},
-
-	/**
-	 * If type is date, dateFormat specifies the format of the date. The format
-	 * specification of the ext library is used:
-	 *
-	 * Format  Output      Description
-	 *	------  ----------  --------------------------------------------------------------
-	 *	  d      10         Day of the month, 2 digits with leading zeros
-	 *	  D      Wed        A textual representation of a day, three letters
-	 *	  j      10         Day of the month without leading zeros
-	 *	  l      Wednesday  A full textual representation of the day of the week
-	 *	  S      th         English ordinal day of month suffix, 2 chars (use with j)
-	 *	  w      3          Numeric representation of the day of the week
-	 *	  z      9          The julian date, or day of the year (0-365)
-	 *	  W      01         ISO-8601 2-digit week number of year, weeks starting on Monday (00-52)
-	 *	  F      January    A full textual representation of the month
-	 *	  m      01         Numeric representation of a month, with leading zeros
-	 *	  M      Jan        Month name abbreviation, three letters
-	 *	  n      1          Numeric representation of a month, without leading zeros
-	 *	  t      31         Number of days in the given month
-	 *	  L      0          Whether its a leap year (1 if it is a leap year, else 0)
-	 *	  Y      2007       A full numeric representation of a year, 4 digits
-	 *	  y      07         A two digit representation of a year
-	 *	  a      pm         Lowercase Ante meridiem and Post meridiem
-	 *	  A      PM         Uppercase Ante meridiem and Post meridiem
-	 *	  g      3          12-hour format of an hour without leading zeros
-	 *	  G      15         24-hour format of an hour without leading zeros
-	 *	  h      03         12-hour format of an hour with leading zeros
-	 *	  H      15         24-hour format of an hour with leading zeros
-	 *	  i      05         Minutes with leading zeros
-	 *	  s      01         Seconds, with leading zeros
-	 *	  O      -0600      Difference to Greenwich time (GMT) in hours
-	 *	  T      CST        Timezone setting of the machine running the code
-	 *	  Z      -21600     Timezone offset in seconds (negative if west of UTC, positive if east)
-	 *
-	 * Example:
-	 *  F j, Y, g:i a  ->  January 10, 2007, 3:05 pm
-	 */
-	dateFormat: function() {
-		return this._jsonProp.dateFormat;
-	},
-
-	/**
-	 * If type is color, this method returns if the fill property should
-	 *  be set.
-	 *  @return {Boolean}
-	 */
-	fill: function() {
-		return this._jsonProp.fill;
-	},
-
-	/**
-	 * If type is color, this method returns if the stroke property should
-	 *  be set.
-	 *  @return {Boolean}
-	 */
-	stroke: function() {
-		return this._jsonProp.stroke;
-	},
-
-	/**
-	 * If type is choice, items is a hash map with all alternative values
-	 * (PropertyItem objects) with id as keys.
-	 */
-	items: function() {
-		return this._items.values();
-	},
-
-	item: function(value) {
-		return this._item[value];
-	},
-
-	toString: function() { return "Property " + this.title() + " (" + this.id() + ")"; },
-
-	// extended by Kerstin (start)
-	complexItems: function() {
-		return this._complexItems.values();
-	},
-	
-	complexItem: function(value) {
-		return this._complexItems[value];
-	}
-	// extended by Kerstin (end)
+    /**
+     * Constructor
+     */
+    construct: function(jsonProp, namespace, stencil){
+        arguments.callee.$.construct.apply(this, arguments);
+        
+        this._jsonProp = jsonProp || ORYX.Log.error("Parameter jsonProp is not defined.");
+        this._namespace = namespace || ORYX.Log.error("Parameter namespace is not defined.");
+        this._stencil = stencil || ORYX.Log.error("Parameter stencil is not defined.");
+        
+        this._items = new Hash();
+        this._complexItems = new Hash();
+        
+        jsonProp.id = jsonProp.id || ORYX.Log.error("ORYX.Core.StencilSet.Property(construct): Id is not defined.");
+        if (!jsonProp.type) {
+            ORYX.Log.info("Type is not defined for stencil '%0', id '%1'. Falling back to 'String'.", stencil, jsonProp.id);
+            jsonProp.type = "string";
+        }
+        else {
+            jsonProp.type = jsonProp.type.toLowerCase();
+        }
+        
+        jsonProp.prefix = jsonProp.prefix || "oryx";
+        jsonProp.title = jsonProp.title || "";
+        jsonProp.value = jsonProp.value || "";
+        jsonProp.description = jsonProp.description || "";
+        jsonProp.readonly = jsonProp.readonly || false;
+        jsonProp.optional = jsonProp.optional || true;
+        
+        //init refToView
+        if (this._jsonProp.refToView) {
+            if (!(this._jsonProp.refToView instanceof Array)) {
+                this._jsonProp.refToView = [this._jsonProp.refToView];
+            }
+        }
+        else {
+            this._jsonProp.refToView = [];
+        }
+        
+        if (jsonProp.min === undefined || jsonProp.min === null) {
+            jsonProp.min = Number.MIN_VALUE;
+        }
+        
+        if (jsonProp.max === undefined || jsonProp.max === null) {
+            jsonProp.max = Number.MAX_VALUE;
+        }
+        
+        if (!jsonProp.fillOpacity) {
+            jsonProp.fillOpacity = false;
+        }
+        
+        if (!jsonProp.strokeOpacity) {
+            jsonProp.strokeOpacity = false;
+        }
+        
+        if (jsonProp.length === undefined || jsonProp.length === null) {
+            jsonProp.length = Number.MAX_VALUE;
+        }
+        
+        if (!jsonProp.wrapLines) {
+            jsonProp.wrapLines = false;
+        }
+        
+        if (!jsonProp.dateFormat) {
+            jsonProp.dataFormat = "m/d/y";
+        }
+        
+        if (!jsonProp.fill) {
+            jsonProp.fill = false;
+        }
+        
+        if (!jsonProp.stroke) {
+            jsonProp.stroke = false;
+        }
+        
+        if (jsonProp.type === ORYX.CONFIG.TYPE_CHOICE) {
+            if (jsonProp.items && jsonProp.items instanceof Array) {
+                jsonProp.items.each((function(jsonItem){
+                    this._items[jsonItem.value] = new ORYX.Core.StencilSet.PropertyItem(jsonItem, namespace, this);
+                }).bind(this));
+            }
+            else {
+                throw "ORYX.Core.StencilSet.Property(construct): No property items defined."
+            }
+            // extended by Kerstin (start)
+        }
+        else 
+            if (jsonProp.type === ORYX.CONFIG.TYPE_COMPLEX) {
+                if (jsonProp.complexItems && jsonProp.complexItems instanceof Array) {
+                    jsonProp.complexItems.each((function(jsonComplexItem){
+                        this._complexItems[jsonComplexItem.id] = new ORYX.Core.StencilSet.ComplexPropertyItem(jsonComplexItem, namespace, this);
+                    }).bind(this));
+                }
+                else {
+                    throw "ORYX.Core.StencilSet.Property(construct): No complex property items defined."
+                }
+            }
+        // extended by Kerstin (end)
+    },
+    
+    /**
+     * @param {ORYX.Core.StencilSet.Property} property
+     * @return {Boolean} True, if property has the same namespace and id.
+     */
+    equals: function(property){
+        return (this._namespace === property.namespace() &&
+        this.id() === property.id()) ? true : false;
+    },
+    
+    namespace: function(){
+        return this._namespace;
+    },
+    
+    stencil: function(){
+        return this._stencil;
+    },
+    
+    id: function(){
+        return this._jsonProp.id;
+    },
+    
+    prefix: function(){
+        return this._jsonProp.prefix;
+    },
+    
+    type: function(){
+        return this._jsonProp.type;
+    },
+    
+    title: function(){
+        return this._jsonProp.title;
+    },
+    
+    value: function(){
+        return this._jsonProp.value;
+    },
+    
+    readonly: function(){
+        return this._jsonProp.readonly;
+    },
+    
+    optional: function(){
+        return this._jsonProp.optional;
+    },
+    
+    description: function(){
+        return this._jsonProp.description;
+    },
+    
+    /**
+     * An optional link to a SVG element so that the property affects the
+     * graphical representation of the stencil.
+     */
+    refToView: function(){
+        return this._jsonProp.refToView;
+    },
+    
+    /**
+     * If type is integer or float, min is the lower bounds of value.
+     */
+    min: function(){
+        return this._jsonProp.min;
+    },
+    
+    /**
+     * If type ist integer or float, max is the upper bounds of value.
+     */
+    max: function(){
+        return this._jsonProp.max;
+    },
+    
+    /**
+     * If type is float, this method returns if the fill-opacity property should
+     *  be set.
+     *  @return {Boolean}
+     */
+    fillOpacity: function(){
+        return this._jsonProp.fillOpacity;
+    },
+    
+    /**
+     * If type is float, this method returns if the stroke-opacity property should
+     *  be set.
+     *  @return {Boolean}
+     */
+    strokeOpacity: function(){
+        return this._jsonProp.strokeOpacity;
+    },
+    
+    /**
+     * If type is string or richtext, length is the maximum length of the text.
+     * TODO how long can a string be.
+     */
+    length: function(){
+        return this._jsonProp.length ? this._jsonProp.length : Number.MAX_VALUE;
+    },
+    
+    wrapLines: function(){
+        return this._jsonProp.wrapLines;
+    },
+    
+    /**
+     * If type is date, dateFormat specifies the format of the date. The format
+     * specification of the ext library is used:
+     *
+     * Format  Output      Description
+     *	------  ----------  --------------------------------------------------------------
+     *	  d      10         Day of the month, 2 digits with leading zeros
+     *	  D      Wed        A textual representation of a day, three letters
+     *	  j      10         Day of the month without leading zeros
+     *	  l      Wednesday  A full textual representation of the day of the week
+     *	  S      th         English ordinal day of month suffix, 2 chars (use with j)
+     *	  w      3          Numeric representation of the day of the week
+     *	  z      9          The julian date, or day of the year (0-365)
+     *	  W      01         ISO-8601 2-digit week number of year, weeks starting on Monday (00-52)
+     *	  F      January    A full textual representation of the month
+     *	  m      01         Numeric representation of a month, with leading zeros
+     *	  M      Jan        Month name abbreviation, three letters
+     *	  n      1          Numeric representation of a month, without leading zeros
+     *	  t      31         Number of days in the given month
+     *	  L      0          Whether its a leap year (1 if it is a leap year, else 0)
+     *	  Y      2007       A full numeric representation of a year, 4 digits
+     *	  y      07         A two digit representation of a year
+     *	  a      pm         Lowercase Ante meridiem and Post meridiem
+     *	  A      PM         Uppercase Ante meridiem and Post meridiem
+     *	  g      3          12-hour format of an hour without leading zeros
+     *	  G      15         24-hour format of an hour without leading zeros
+     *	  h      03         12-hour format of an hour with leading zeros
+     *	  H      15         24-hour format of an hour with leading zeros
+     *	  i      05         Minutes with leading zeros
+     *	  s      01         Seconds, with leading zeros
+     *	  O      -0600      Difference to Greenwich time (GMT) in hours
+     *	  T      CST        Timezone setting of the machine running the code
+     *	  Z      -21600     Timezone offset in seconds (negative if west of UTC, positive if east)
+     *
+     * Example:
+     *  F j, Y, g:i a  ->  January 10, 2007, 3:05 pm
+     */
+    dateFormat: function(){
+        return this._jsonProp.dateFormat;
+    },
+    
+    /**
+     * If type is color, this method returns if the fill property should
+     *  be set.
+     *  @return {Boolean}
+     */
+    fill: function(){
+        return this._jsonProp.fill;
+    },
+    
+    /**
+     * If type is color, this method returns if the stroke property should
+     *  be set.
+     *  @return {Boolean}
+     */
+    stroke: function(){
+        return this._jsonProp.stroke;
+    },
+    
+    /**
+     * If type is choice, items is a hash map with all alternative values
+     * (PropertyItem objects) with id as keys.
+     */
+    items: function(){
+        return this._items.values();
+    },
+    
+    item: function(value){
+        return this._item[value];
+    },
+    
+    toString: function(){
+        return "Property " + this.title() + " (" + this.id() + ")";
+    },
+    
+    // extended by Kerstin (start)
+    complexItems: function(){
+        return this._complexItems.values();
+    },
+    
+    complexItem: function(value){
+        return this._complexItems[value];
+    }
+    // extended by Kerstin (end)
 });
