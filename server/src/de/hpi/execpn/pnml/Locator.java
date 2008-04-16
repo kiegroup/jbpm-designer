f96c8bf3e74dada521034d4daa411c22a1696b01
lutz.gericke <lutz.gericke@d672c736-503d-0410-a38a-9366997c882b>
2008-04-16 14:12:06 +0000

    git-svn-id: http://oryx-editor.googlecode.com/svn/trunk@166 d672c736-503d-0410-a38a-9366997c882b

---
+++ server/src/de/hpi/execpn/pnml/Locator.java
@@ -0,0 +1,31 @@
+package de.hpi.execpn.pnml;
+
+public class Locator {
+	private String name;
+	private String datatype;
+	private String xpath;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDatatype() {
+		return datatype;
+	}
+	public void setDatatype(String datatype) {
+		this.datatype = datatype;
+	}
+	public String getXpath() {
+		return xpath;
+	}
+	public void setXpath(String xpath) {
+		this.xpath = xpath;
+	}
+	public Locator(String name, String datatype, String xpath) {
+		this.name = name;
+		this.datatype = datatype;
+		this.xpath = xpath;
+	}
+}
