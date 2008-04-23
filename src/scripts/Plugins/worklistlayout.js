735af7df2721f12d880a6a3ad0379e921efa27ec
lutz.gericke <lutz.gericke@d672c736-503d-0410-a38a-9366997c882b>
2008-04-23 11:21:31 +0000

    - added toolbar for task actions - fetch xml data directly from engine with xmlreader through proxy servlet
    git-svn-id: http://oryx-editor.googlecode.com/svn/trunk@201 d672c736-503d-0410-a38a-9366997c882b

--- src/scripts/Plugins/worklistlayout.js
+++ src/scripts/Plugins/worklistlayout.js
@@ -8,13 +8,15 @@ function loadStoreForCase(val) {
 	Ext.task_grid.render();
 }
 
+Ext.engine_url = 'http://localhost:3000/';
+
 Ext.onReady(function() {
 var xg = Ext.grid;
 
 Ext.button_toolbar = new Ext.Toolbar({width:'100%', height:'100%', autoFit: true});
 
-var store = new Ext.data.Store({
-    url: '/oryx/engineproxy?url=http://localhost:3000/cases',
+Ext.store = new Ext.data.Store({
+    url: '/oryx/engineproxy?url=' + Ext.engine_url + 'cases',
 
     reader: new Ext.data.XmlReader({
            record: 'task',
@@ -25,24 +27,26 @@ var store = new Ext.data.Store({
        ])
 });
 
-var store2 = new Ext.data.Store({
-    url: '/oryx/engineproxy?url=http://localhost:3000/cases/217',
-
+Ext.store2 = new Ext.data.GroupingStore({
+    url: '/oryx/engineproxy?url=' + Ext.engine_url + 'cases/217',
+	groupField: 'transition_name',
+	sortInfo: {field: 'transition_name', direction: 'ASC'},
     reader: new Ext.data.XmlReader({
            record: 'task',
-           id: 'transition_id',
+           id: 'transition_name',
            totalRecords: '@total'
        }, [
-           'transition_id', 'transition_name', 'action'
+           'transition_id', 'transition_name' , 'action', 'case_id'
        ])
 });
 
+
 Ext.case_grid = new Ext.grid.GridPanel({
-						        store: store,
+						        store: Ext.store,
 						        columns: [
 						            {header: "case_id", dataIndex: 'case_id', sortable: true, renderer: caseLinkRenderer}
 						        ],
-						        width: 200,
+						        width: '100%',
 						        height: 400,
 				                viewConfig: {
 										        forceFit: true,
@@ -50,12 +54,13 @@ Ext.case_grid = new Ext.grid.GridPanel({
 										    }
 						        });
 		
-Ext.task_grid = 			                new xg.GridPanel({
-								store: store2,							
+Ext.task_grid = new xg.GridPanel({
+								store: Ext.store2,							
 								columns: [ 
-									{id: 'transition_id', header: 'transition id', sortable: true, dataIndex: 'transition_id', renderer: myCustomRenderer},
-									{header: 'transition name', sortable: true, dataIndex: 'transition_name'},
-									{header: 'action', sortable: true, dataIndex: 'action'}
+//									{id: 'transition_id', header: 'transition id', sortable: true, dataIndex: 'transition_id', renderer: transitionLinkRenderer},
+									{header: 'transition name', sortable: true, dataIndex: 'transition_name'}
+//									,
+//									{header: 'action', sortable: true, dataIndex: 'action'}
 								],
 				                viewConfig: {
 										        forceFit: true,
@@ -63,19 +68,101 @@ Ext.task_grid = 			                new xg.GridPanel({
 										    },
 							         height: 200,
 							         collapsible: false,
-							         iconCls: 'icon-grid'
+							         iconCls: 'icon-grid',
+									 onClick: taskOnClick
 							   });				        
 
-function myCustomRenderer(value){
-	return String.format('<a href="http://localhost:3000/transitions/{0}" target="form_frame">DO</a>', value);
+function transitionLinkRenderer(value){
+	return String.format('<a href="' + Ext.engine_url + '/transitions/{0}" target="form_frame">DO</a>', value);
 }
 
 function caseLinkRenderer(value) {
 	return String.format('<a href="javascript:loadStoreForCase({0})">Case {0}</a>', value); 
 }
 
+
+function taskOnClick(blub) {
+	Ext.allocateBtn.setDisabled(true);
+	Ext.submitBtn.setDisabled(true);
+	Ext.suspendBtn.setDisabled(true);
+	Ext.resumeBtn.setDisabled(true);
+	Ext.delegateBtn.setDisabled(true);
+	Ext.reviewBtn.setDisabled(true);
+	Ext.skipBtn.setDisabled(true);
+	
+	
+	Ext.actionsStore = new Ext.data.Store({
+	    url: '/oryx/engineproxy?url=' + Ext.engine_url + 'cases/'+Ext.task_grid.selModel.getSelected().data.case_id,
+	    reader: new Ext.data.XmlReader({
+	           record: 'task',
+	           id: 'transition_id',
+	           totalRecords: '@total'
+	       }, [
+	           'action', 'transition_name', 'transition_id'
+	       ])
+	});
+	
+	Ext.actionsStore.on('load', function(a,b,c) {
+		Ext.actionsStore.each(function(rec) {
+		if (Ext.task_grid.selModel.getSelected().data.transition_name == rec.data.transition_name) {
+			switch (rec.data.action)
+				{
+				case 'allocate':
+				  Ext.allocateBtn.transition_id = rec.data.transition_id;
+				  Ext.allocateBtn.setDisabled(false);
+				  break
+				case 'submit':
+				  Ext.submitBtn.transition_id = rec.data.transition_id;
+				  Ext.submitBtn.setDisabled(false);
+				  break
+				case 'suspend':
+				  Ext.suspendBtn.transition_id = rec.data.transition_id;
+				  Ext.suspendBtn.setDisabled(false);
+				  break
+				case 'resume':
+				  Ext.resumeBtn.transition_id = rec.data.transition_id;
+				  Ext.resumeBtn.setDisabled(false);
+				  break
+				case 'delegate':
+				  Ext.delegateBtn.transition_id = rec.data.transition_id;
+				  Ext.delegateBtn.setDisabled(false);
+				  break
+				case 'review':
+				  Ext.reviewBtn.transition_id = rec.data.transition_id;
+				  Ext.reviewBtn.setDisabled(false);
+				  break
+				case 'skip':
+				  Ext.skipBtn.transition_id = rec.data.transition_id;
+				  Ext.skipBtn.setDisabled(false);
+				  break
+				  }
+			}
+		});
+	}, this);
+	
+	Ext.actionsStore.load();
+	
+	
+//	Ext.msg.alert('info', this.selModel.getSelected().data.action);
+//	Ext.reviewBtn.setDisabled(Ext.reviewBtn.disabled == true ? false : true);
+}
+
+function buttonOnClick(blub) {
+	var transition_id = this.transition_id;
+	
+	form_frame.location = Ext.engine_url + 'transitions/' + transition_id;
+}
+
 Ext.viewport = new Ext.Viewport({
             layout:'border',
+            buttons: [{
+          text: "OK",
+          id: "buttonOK",
+          type: "submit",
+          handler: function() {
+              Ext.msg.alert('test','blub');
+           }
+       }],
             items:[{
                 region:'west',
                 id:'west-panel',
@@ -176,16 +263,25 @@ Ext.viewport = new Ext.Viewport({
                 
         });
         
-        store.load();        
-        store2.load();
-        Ext.store = store;
-        Ext.store2 = store2;     
+        Ext.store.load();        
+        Ext.store2.load();
         
-        Ext.button_toolbar.add(new Ext.Button({text: 'allocate'}));
-        Ext.button_toolbar.add(new Ext.Button({text: 'submit'}));
-        Ext.button_toolbar.add(new Ext.Button({text: 'suspend'}));
-        Ext.button_toolbar.add(new Ext.Button({text: 'resume'}));
-        Ext.button_toolbar.add(new Ext.Button({text: 'delegate'}));
-        Ext.button_toolbar.add(new Ext.Button({text: 'review'}));
+        Ext.allocateBtn = new Ext.Button({id: 'allocate', text: 'allocate', onClick: buttonOnClick});
+        Ext.allocateBtn.setDisabled(true);
+        Ext.submitBtn = new Ext.Button({id: 'submit', text: 'submit', onClick: buttonOnClick});
+        Ext.submitBtn.setDisabled(true);
+        Ext.suspendBtn = new Ext.Button({id: 'suspend', text: 'suspend', onClick: buttonOnClick});
+        Ext.resumeBtn = new Ext.Button({id: 'resume', text: 'resume', onClick: buttonOnClick});
+        Ext.delegateBtn = new Ext.Button({id: 'delegate', text: 'delegate', onClick: buttonOnClick});
+        Ext.reviewBtn = new Ext.Button({id: 'review', text: 'review', onClick: buttonOnClick});
+        Ext.skipBtn = new Ext.Button({id: 'skip', text: 'skip', onClick: buttonOnClick});
+        
+        Ext.button_toolbar.add(Ext.allocateBtn);
+        Ext.button_toolbar.add(Ext.submitBtn);
+        Ext.button_toolbar.add(Ext.suspendBtn);
+        Ext.button_toolbar.add(Ext.resumeBtn);
+        Ext.button_toolbar.add(Ext.delegateBtn);
+        Ext.button_toolbar.add(Ext.reviewBtn);
+        Ext.button_toolbar.add(Ext.skipBtn);
            
 });
