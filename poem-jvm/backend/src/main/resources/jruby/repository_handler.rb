2e2f0f446b8288bbde82730f7de76169b369ea3f
ole.eckermann@student.hpi.uni-potsdam.de <ole.eckermann@student.hpi.uni-potsdam.de@d672c736-503d-0410-a38a-9366997c882b>
2008-04-29 17:39:11 +0000

    license comments added; hard coded icon-uris for stencil sets
    git-svn-id: http://oryx-editor.googlecode.com/svn/trunk@220 d672c736-503d-0410-a38a-9366997c882b

--- poem-jvm/backend/src/main/resources/jruby/repository_handler.rb
+++ poem-jvm/backend/src/main/resources/jruby/repository_handler.rb
@@ -1,3 +1,26 @@
+##############################
+ # Copyright (c) 2008
+ # Ole Eckermann
+ #
+ # Permission is hereby granted, free of charge, to any person obtaining a
+ # copy of this software and associated documentation files (the "Software"),
+ # to deal in the Software without restriction, including without limitation
+ # the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ # and/or sell copies of the Software, and to permit persons to whom the
+ # Software is furnished to do so, subject to the following conditions:
+ #
+ # The above copyright notice and this permission notice shall be included in
+ # all copies or substantial portions of the Software.
+ #
+ # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ # DEALINGS IN THE SOFTWARE.
+ ##############################
+
 module Handler
   class RepositoryHandler < DefaultHandler
     def doGet(interaction)
@@ -40,10 +63,9 @@ module Handler
       
       output = ActiveSupport::JSON.encode(
       [
-          {:id => "bpmn", :title => "BPMN", :description => "Business Process Model Notation", :uri => "/stencilsets/bpmn/bpmn.json", :icon_url => "/poem-backend-1.0/images/silk/flag_orange.png"},
-          {:id => "petrinet", :title => "Petri Net", :description => "Petri Net", :uri => "/stencilsets/petrinets/petrinet.json", :icon_url => "/poem-backend-1.0/images/silk/flag_green.png"},
-          {:id => "epc", :title => "EPC", :description => "Event-Driven Process Chain", :uri => "/stencilsets/epc/epc.json", :icon_url => "/poem-backend-1.0/images/silk/flag_red.png"},
-          {:id => "workflow", :title => "Workflow Net", :description => "Workflow Net", :uri => "/stencilsets/workflownets/workflownets.json", :icon_url => "/poem-backend-1.0/images/silk/flag_blue.png"}
+          {:id => "bpmn", :title => "BPMN", :description => "Business Process Model Notation", :uri => "/stencilsets/bpmn/bpmn.json", :icon_url => "/oryx/stencilsets/bpmn/bpmn.png"},
+          {:id => "petrinet", :title => "Petri Net", :description => "Petri Net", :uri => "/stencilsets/petrinets/petrinet.json", :icon_url => "/oryx/stencilsets/petrinets/petrinets.png"},
+          {:id => "epc", :title => "EPC", :description => "Event-Driven Process Chain", :uri => "/stencilsets/epc/epc.json", :icon_url => "/oryx/stencilsets/epc/epc.png"}
       ])
       out.print(output);
 
