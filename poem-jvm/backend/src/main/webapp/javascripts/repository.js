7dda3e5b6d6b4b3e9436b3d571c59b7511769ff1
W.Tscheschner <W.Tscheschner@d672c736-503d-0410-a38a-9366997c882b>
2008-05-07 16:08:38 +0000

    git-svn-id: http://oryx-editor.googlecode.com/svn/trunk@281 d672c736-503d-0410-a38a-9366997c882b

--- poem-jvm/backend/src/main/webapp/javascripts/repository.js
+++ poem-jvm/backend/src/main/webapp/javascripts/repository.js
@@ -649,12 +649,10 @@ Repository.render = {
 				'<img src="/poem-backend-1.0/images/style/oryx.small.gif" id="oryx_repository_logo" alt="ORYX Logo" title="ORYX"/>',
 		
 				'<tpl if="this.isAnonymousUser(current_user) || this.isPublic(current_user)">',
-// disabled tooltip					'<form action="/poem-backend-1.0/consumer" method="post" id="openid_login" onmouseover="$(\'openid_label\').style.display=\'block\';" onmouseout="$(\'openid_label\').style.display=\'none\';">',
 					'<form action="/poem-backend-1.0/consumer" method="post" id="openid_login">',
 						'<div>',
 							'<input type="text" name="openid_identifier" id="openid_login_openid" class="text gray" value="your.openid.net" onblur="if(this.value.replace(/^\s+/, \'\').replace(/\s+$/, \'\').length==0) {this.value=\'your.openid.net\'; this.className+=\' gray\';}" onfocus="this.className = this.className.replace(/ gray/ig, \'\'); if(this.value==\'your.openid.net\') this.value=\'\';" />',
 							'<input type="submit" class="button" value="login"/>',
-//	disabled tooltip						'<label id="openid_label" for="openid_login_openid">Enter your <a href="http://openid.net">open id</a> to log in.<br/> Don\'t have one? <a href="/Oryx/OpenID" class="twikiLink">Get one!</a></label>',
 						'</div>',
 					'</form>',
 				'</tpl>',
