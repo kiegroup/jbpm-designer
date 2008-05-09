edee0a3356e9b9eb99ef2a90312220fbab42f4c0
martin.czuchra <martin.czuchra@d672c736-503d-0410-a38a-9366997c882b>
2008-05-09 13:33:13 +0000

    * More debug output in layouter. * Only one debug flag. * No more parenthesis on filename parameters, this breaks Unix systems.
    git-svn-id: http://oryx-editor.googlecode.com/svn/trunk@295 d672c736-503d-0410-a38a-9366997c882b

--- server/src/org/oryxeditor/server/AutoLayouterServlet.java
+++ server/src/org/oryxeditor/server/AutoLayouterServlet.java
@@ -13,117 +13,198 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 //Thread for continuously reading a process' InputStream
 //needed because java processes can deadlock, when InputStream exceeds a certain threshold 
-class StreamReadThread extends Thread
-{
-	public static final boolean debug = false;
-	
+class StreamReadThread extends Thread {
+
     InputStream is;
     String content;
     Boolean stop;
-    
-    StreamReadThread(InputStream is)
-    {
-        this.is = is;
-        this.content = "";
-        this.stop = false;        
-        
+
+    StreamReadThread(InputStream is) {
+	this.is = is;
+	this.content = "";
+	this.stop = false;
+
     }
-    
-    public void run()
-    {
-        try
-        {
-        	if (debug){System.out.println("Auto-Layouter: Reader Thread started");}
-            InputStreamReader isr = new InputStreamReader(is);
-            BufferedReader br = new BufferedReader(isr);
-            String line=null;
-            if (debug){ System.out.println("Auto-Layouter: Reader Thread yields");}
-            yield();
-            if (debug){ System.out.println("Auto-Layouter: Reader Thread starts reading");}
-            while(!stop){
-            	if ( (line = br.readLine()) != null){
-            		content += line;
-            	}
-            	else{
-            		if (debug){System.out.println("Auto-Layouter: Reader Thread sleeps");}
-            		sleep(200);
-            	}
-            	
-            }
-                //System.out.println( line);    
-            } catch (Exception e)
-              {
-                e.printStackTrace();
-                content += "\nException: " + e.toString() + " occured!";
-              }
-            if (debug){System.out.println("Auto-Layouter: Reader Thread stopped");}
+
+    public void run() {
+	try {
+	    if (AutoLayouterServlet.DEBUG) {
+		System.out.println("Auto-Layouter: Reader Thread started");
+	    }
+	    InputStreamReader isr = new InputStreamReader(is);
+	    BufferedReader br = new BufferedReader(isr);
+	    String line = null;
+	    if (AutoLayouterServlet.DEBUG) {
+		System.out.println("Auto-Layouter: Reader Thread yields");
+	    }
+	    yield();
+	    if (AutoLayouterServlet.DEBUG) {
+		System.out
+			.println("Auto-Layouter: Reader Thread starts reading");
+	    }
+	    while (!stop) {
+		if ((line = br.readLine()) != null) {
+		    content += line;
+		} else {
+		    if (AutoLayouterServlet.DEBUG) {
+			System.out
+				.println("Auto-Layouter: Reader Thread sleeps");
+		    }
+		    sleep(200);
+		}
+
+	    }
+	    // System.out.println( line);
+	} catch (Exception e) {
+	    e.printStackTrace();
+	    content += "\nException: " + e.toString() + " occured!";
+	}
+	if (AutoLayouterServlet.DEBUG) {
+	    System.out.println("Auto-Layouter: Reader Thread stopped");
+	}
     }
-    
-    public void stop_reading(){
-    	stop = true;
-    	if (debug){System.out.println("Auto-Layouter: Stopping Reader Thread");}
+
+    public void stop_reading() {
+	stop = true;
+	if (AutoLayouterServlet.DEBUG) {
+	    System.out.println("Auto-Layouter: Stopping Reader Thread");
+	}
     }
 }
+
 public class AutoLayouterServlet extends HttpServlet {
 
-	public static final boolean debug = false;
-	private static final long serialVersionUID = -1255777265795121521L;
+    public static final boolean DEBUG = true;
+    private static final long serialVersionUID = -1255777265795121521L;
 
-	protected void wait_for_thread(StreamReadThread thread){
-		thread.stop_reading();
-		if (debug){System.out.println("Auto-Layouter: Waiting for reader thread");}
-		try {
-		thread.join();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+    protected void wait_for_thread(StreamReadThread thread) {
+	thread.stop_reading();
+	if (DEBUG) {
+	    System.out.println("Auto-Layouter: Waiting for reader thread");
 	}
-	
-	protected void doPost(HttpServletRequest req, HttpServletResponse res)
-			throws ServletException, IOException {
+	try {
+	    thread.join();
+	} catch (InterruptedException e) {
+	    e.printStackTrace();
+	}
+    }
 
-		String rdf = req.getParameter("rdf");
-		String basefilename = String.valueOf(System.currentTimeMillis());
-		
-		String tmpRdfFile = this.getServletContext().getRealPath("/")
-				+ "tmp" + File.separator + basefilename
-				+ ".rdf";
-		BufferedWriter out = new BufferedWriter(new FileWriter(tmpRdfFile));
-		out.write(rdf);
-		out.close();
-		if (debug){System.out.println("Auto-Layouter: Saved RDF to " + tmpRdfFile);}
-		
-		String layouter_path = this.getServletContext().getRealPath("/")
-		+ "Plugins" + File.separator + "AutoLayouter" + File.separator + "src"+ File.separator + "startlayout.rb";
-		ProcessBuilder builder = new ProcessBuilder("ruby", "\"" + layouter_path + "\"", "\"" + tmpRdfFile + "\"");
-		builder.redirectErrorStream(true);
-		
-		if (debug){System.out.println("Auto-Layouter: Calling ruby with: ruby \"" + layouter_path + "\" \"" + tmpRdfFile + "\"");}
-		Process p = builder.start();
-		if (debug){System.out.println("Auto-Layouter: Called Ruby, Starting ReaderThread");}
-		
-		StreamReadThread reader = new StreamReadThread(p.getInputStream());
-		reader.start();
-		res.setContentType("text/html");
-		try {
-			if (debug){System.out.println("Auto-Layouter: Waiting for Ruby to return");}
-			if (p.waitFor() != 0){
-				if (debug){System.out.println("Auto-Layouter: Ruby returned an error");}
-				res.getWriter().println("{'error':'Layouter returned an error'}");
-				wait_for_thread(reader);
-				System.err.println(reader.content);
-				return;
-			}
-			if (debug){System.out.println("Auto-Layouter: Ruby returned");}
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-			System.out.println("Wait interrupted");
+    protected void doPost(HttpServletRequest req, HttpServletResponse res)
+	    throws ServletException, IOException {
+
+	if (DEBUG) {
+
+	    // execute 'which ruby' in the context of the servlet container.
+	    try {
+
+		// start a process for 'which ruby' and wait for it.
+		Process which = Runtime.getRuntime().exec("which ruby");
+		which.waitFor();
+
+		// get the output in a buffered reader.
+		BufferedReader whichIn = new BufferedReader(
+			new InputStreamReader(which.getInputStream()));
+
+		// output the debug information.
+		System.out.print("ruby location: ");
+		System.out.println(whichIn.readLine());
+
+	    } catch (InterruptedException e1) {
+
+		// if something goes wrong, tell us.
+		e1.printStackTrace();
+	    }
+	}
+
+	// get rdf and timestamp for the temporary file.
+	String rdf = req.getParameter("rdf");
+	String basefilename = String.valueOf(System.currentTimeMillis());
+
+	// create a new temporary file and store the rdf.
+	String tmpRdfFile = this.getServletContext().getRealPath("/") + "tmp"
+		+ File.separator + basefilename + ".rdf";
+	BufferedWriter out = new BufferedWriter(new FileWriter(tmpRdfFile));
+	out.write(rdf);
+	out.close();
+
+	// get the path to the layouter.
+	String layouter_path = this.getServletContext().getRealPath("/")
+		+ "Plugins" + File.separator + "AutoLayouter" + File.separator
+		+ "src" + File.separator + "startlayout.rb";
+
+	if (DEBUG) {
+
+	    System.out.println("Auto-Layouter: Saved RDF to " + tmpRdfFile);
+
+	    // try to find the layouter.
+	    try {
+
+		// start a process for 'ls %layouter_path%' and wait for it.
+		Process ls = Runtime.getRuntime().exec("ls " + layouter_path);
+		ls.waitFor();
+
+		// get the output in a buffered reader.
+		BufferedReader lsIn = new BufferedReader(new InputStreamReader(
+			ls.getInputStream()));
+
+		// output the debug information.
+		System.out.print("ls on layouter: ");
+		System.out.println(lsIn.readLine());
+
+	    } catch (InterruptedException e1) {
+
+		// if something goes wrong, tell us.
+		e1.printStackTrace();
+	    }
+	}
+
+	ProcessBuilder builder = new ProcessBuilder("ruby", layouter_path,
+		tmpRdfFile);
+	builder.redirectErrorStream(true);
+	;
+
+	if (DEBUG) {
+	    System.out.println("Auto-Layouter: Calling ruby with: "
+		    + builder.command());
+	}
+	Process p = builder.start();
+
+	if (DEBUG) {
+	    System.out
+		    .println("Auto-Layouter: Called Ruby, Starting ReaderThread");
+	}
+
+	StreamReadThread reader = new StreamReadThread(p.getInputStream());
+	reader.start();
+	res.setContentType("text/html");
+	try {
+	    if (DEBUG) {
+		System.out.println("Auto-Layouter: Waiting for Ruby to return");
+	    }
+	    if (p.waitFor() != 0) {
+		if (DEBUG) {
+		    System.out.println("Auto-Layouter: Ruby returned an error");
 		}
+		res.getWriter().println(
+			"{'error':'Layouter returned an error:\n"
+				+ reader.content + "'}");
 		wait_for_thread(reader);
-		if (debug){System.out.println("Auto-Layouter: Sending response");}
-		res.getWriter().println(reader.content);
+		System.err.println(reader.content);
+		return;
+	    }
+	    if (DEBUG) {
+		System.out.println("Auto-Layouter: Ruby returned");
+	    }
+	} catch (InterruptedException e) {
+	    e.printStackTrace();
+	    System.out.println("Wait interrupted");
 	}
+	wait_for_thread(reader);
+	if (DEBUG) {
+	    System.out.println("Auto-Layouter: Sending response");
+	}
+	res.getWriter().println(reader.content);
+    }
 }
