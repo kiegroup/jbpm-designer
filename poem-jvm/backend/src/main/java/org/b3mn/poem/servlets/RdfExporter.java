baec2d0a03fe0cd7388ba114aad59371ed9c70bc
ole.eckermann@student.hpi.uni-potsdam.de <ole.eckermann@student.hpi.uni-potsdam.de@d672c736-503d-0410-a38a-9366997c882b>
2008-06-13 12:01:34 +0000

    Missing Transaction.commit() in RdfExporter added
    git-svn-id: http://oryx-editor.googlecode.com/svn/trunk@522 d672c736-503d-0410-a38a-9366997c882b

--- poem-jvm/backend/src/main/java/org/b3mn/poem/servlets/RdfExporter.java
+++ poem-jvm/backend/src/main/java/org/b3mn/poem/servlets/RdfExporter.java
@@ -8,14 +8,19 @@ import javax.servlet.http.HttpServletResponse;
 import javax.xml.transform.TransformerException;
 
 import org.b3mn.poem.Identity;
+import org.b3mn.poem.Persistance;
+
 public class RdfExporter {
 
 	public void doGet(HttpServletRequest req, HttpServletResponse res, Identity subject, Identity object, String hostname) throws IOException {
-  		res.setContentType("text/xml");
+		
+		res.setContentType("text/xml");
   		res.setStatus(200);	
   		try {
+  			String rdfRepresentation = object.read().getRdf();
+  			Persistance.commit();
     		PrintWriter out = res.getWriter();
-			out.write(object.read().getRdf());
+			out.write(rdfRepresentation);
 		} catch (TransformerException e) {
 			e.printStackTrace();
 		}
