5b4b52d4af9a5691b5187c90ba05ed70b5f22aa2
W.Tscheschner <W.Tscheschner@d672c736-503d-0410-a38a-9366997c882b>
2008-08-05 11:51:31 +0000

    Started to integrate the Atlas project to the oryx
    git-svn-id: http://oryx-editor.googlecode.com/svn/trunk@785 d672c736-503d-0410-a38a-9366997c882b

---
+++ server/src/org/oryxeditor/server/AMLSupport.java
@@ -0,0 +1,190 @@
+package org.oryxeditor.server;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import com.sun.org.apache.xerces.internal.parsers.DOMParser;
+
+import de.hpi.bpt.epc.EPC;
+import de.hpi.bpt.epc.aml.util.AMLParser;
+import de.hpi.bpt.epc.aml.util.OryxSerializer;
+
+
+/**
+ * Copyright (c) 2008 Willi Tscheschner
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+public class AMLSupport extends HttpServlet {
+
+	private static final long serialVersionUID = 316274845723034029L;
+	
+    /**
+     * The POST request.EPCUpload.java
+     */
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException {
+    	
+    	// Get the PrintWriter
+    	res.setContentType("text/html");
+    	PrintWriter out = null;
+    	try {
+    	    out = res.getWriter();
+    	} catch (IOException e) {
+    	    e.printStackTrace();
+    	}
+    	
+    	// No isMultipartContent => Error
+    	final boolean isMultipartContent = ServletFileUpload.isMultipartContent(req);
+    	if (!isMultipartContent){
+    		printError(out, "No Multipart Content transmitted.");
+			return ;
+    	}
+    	
+    	// Get the uploaded file
+    	final FileItemFactory factory = new DiskFileItemFactory();
+    	final ServletFileUpload servletFileUpload = new ServletFileUpload(factory);
+    	servletFileUpload.setSizeMax(-1);
+    	final List<?> items;
+    	try {
+    		items = servletFileUpload.parseRequest(req);
+    		if (items.size() != 1){
+    			printError(out, "Not exactly one File.");
+    			return ;
+    		}
+    	} catch (FileUploadException e) {
+    		handleException(out, e); 
+	   		return;
+    	} 
+    	
+    	final FileItem fileItem = (FileItem)items.get(0);
+
+    	
+    	try {
+			DocumentBuilder builder;
+			DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+			builder = docFactory.newDocumentBuilder();
+			Document document = builder.parse(new ByteArrayInputStream(fileItem.get()));
+						
+			String content = document.getTextContent();
+			out.print("{success:true, content:'"+ content +"'}");
+			
+			/*AMLParser parser = new AMLParser(fileItem.getName());
+			parser.parse(document);
+			Collection<EPC> epcs = new HashSet<EPC>();
+			Iterator<String> ids = parser.getModelIds().iterator();
+			while (ids.hasNext()) {
+				String modelId = ids.next();
+				epcs.add(parser.getEPC(modelId));
+			}
+			
+			OryxSerializer oryxSerializer = new OryxSerializer(epcs);
+			oryxSerializer.parse();
+
+			Document outputDocument = oryxSerializer.getDocument();
+*/
+	//		out.print("{success:true, content:'"+ outputDocument.getTextContent() +"'}");
+			
+		} catch (Exception e) {
+			handleException(out, e); 
+    		return;
+		}    
+		
+
+		
+		
+    	/*content = content.replaceAll("<", "&lt;");
+		content = content.replaceAll(">", "&gt;");
+		content = content.replaceAll("\n", "");
+		
+    	String resultString = inputStream.toString();
+    	out.print("{success:true, content:'"+fileName+" -- " + content +"'}"); */
+    	
+    	/*
+    	if (resultString != null){
+    		try {
+    			if (resultString.startsWith("<?xml version=\"1.0\" encoding=\"UTF-8\"?><root>")){
+    				resultString = resultString.replace("<?xml version=\"1.0\" encoding=\"UTF-8\"?><root>", "");
+    				resultString = resultString.replace("</root>", "");
+
+    		        out.print("{success:true, content:'"+resultString+"'}"); 
+    		        return ;
+    			} else if (resultString.startsWith("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<root>")) {
+    				resultString = resultString.replace("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<root>", "");
+    				resultString = resultString.replace("</root>", "");
+    				resultString = resultString.replaceAll("<", "&lt;");
+    				resultString = resultString.replaceAll(">", "&gt;");
+    		        out.print("{success:true, content:'"+resultString+"'}"); 
+    		        return ;
+    			} else {
+    				printError(out, "Error during transformation.");
+    				return ;
+    			}
+
+    		} catch (Exception e){
+    			handleException(out, e); 
+    			return;
+    		}
+    	}*/
+    }
+    
+    
+    
+    private void printError(PrintWriter out, String err){
+    	if (out != null){
+    		out.print("{success:false, content:'"+err+"'}");
+
+    	}
+    }
+    
+	private void handleException(PrintWriter out, Exception e) {
+		e.printStackTrace();
+		printError(out, e.getLocalizedMessage());
+	}
+    
+}
---
+++ src/scripts/Plugins/amlSupport.js
@@ -0,0 +1,321 @@
+/**
+ * Copyright (c) 2008
+ * Willi Tscheschner
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ **/
+
+if(!ORYX.Plugins)
+	ORYX.Plugins = new Object();
+
+/**
+ * Supports EPCs by offering a syntax check and export and import ability..
+ * 
+ * 
+ */
+ORYX.Plugins.AMLSupport = Clazz.extend({
+
+	facade: undefined,
+
+	/**
+	 * Offers the plugin functionality:
+	 * 
+	 */
+	construct: function(facade) {
+		this.facade = facade;
+			
+		this.facade.offer({
+			'name':				"Import from AML",
+			'functionality': 	this.importAML.bind(this),
+			'group': 			"epc",
+			'icon': 			ORYX.PATH + "images/epc_import.png",
+			'description': 		"Import an AML file",
+			'index': 			3,
+			'minShape': 		0,
+			'maxShape': 		0
+		});
+
+	
+		this.AMLServletURL = '/amlsupport';
+	},
+
+	
+	/**
+	 * Imports an AML description
+	 */
+	importAML: function(){
+		this.openUploadDialog();
+	},		
+
+	/**
+	 * Opens a upload dialog.
+	 * 
+	 */
+	openUploadDialog: function(){
+
+		var form = new Ext.form.FormPanel({
+			frame : true,
+            bodyStyle: 'padding:5px',
+			defaultType : 'textfield',
+		 	waitMsgTarget : true,
+		  	labelAlign : 'left',
+		  	buttonAlign: 'right',
+		  	fileUpload : true,
+		  	enctype : 'multipart/form-data',
+		  	items : [
+		  	{
+		    	text : 		'Select an AML (.aml) file to import it!', 
+				style : 	'font-size:12px;margin-bottom:10px;display:block;',
+				xtype : 	'label'
+		  	},{
+		    	fieldLabel : 	'File',
+		    	inputType : 	'file',
+				labelStyle :	'width:50px'
+		  	}]
+		});
+
+		var dialog = new Ext.Window({ 
+			autoCreate: true, 
+			title: 		'Import AML-File', 
+			height: 	'auto', 
+			width: 		400, 
+			modal:		true,
+			collapsible:false,
+			fixedcenter:true, 
+			shadow:		true, 
+			proxyDrag: 	true,
+			resizable:	false,
+			items: [form],
+			buttons:[
+				{
+					text:'Import',
+					handler:function(){
+						
+						form.form.submit({
+				      						url: 		ORYX.PATH + this.AMLServletURL,
+				      						waitMsg: 	"Importing...",
+				      						success: 	function(f,a){
+													dialog.hide();
+													console.log(a.result.content)
+													/*
+													var erdf = a.result.content;
+													erdf = erdf.replace(/&lt;/g, "<");
+													erdf = erdf.replace(/&gt;/g, ">");
+													erdf = '<?xml version="1.0" encoding="utf-8"?><div>'+erdf+'</div>';	
+													this.loadContent(erdf);*/
+				      							}.bind(this),
+											failure: 	function(f,a){
+													dialog.hide();
+													
+													console.log('Error',f,a)
+													/*
+													Ext.MessageBox.show({
+						           						title: 		'Error',
+						          	 					msg: 		'Error',//a.response.responseText.substring(a.response.responseText.indexOf("content:'")+9, a.response.responseText.indexOf("'}")),
+						           						buttons: 	Ext.MessageBox.OK,
+						           						icon: 		Ext.MessageBox.ERROR
+						       						});*/
+								      		}	
+										});					
+					}.bind(this)
+				},{
+					text:'Close',
+					handler:function(){
+						dialog.hide();
+					}.bind(this)
+				}
+			]
+		});
+		
+		dialog.on('hide', function(){
+			dialog.destroy(true);
+			delete dialog;
+		});
+		dialog.show();
+	},
+	
+	loadContent: function(erdf){
+		
+		console.log(erdf);
+
+	},
+	
+	/**
+	 * 
+	 * THE FOLLOWING METHODS ARE A TEMPORARY SOLUTION
+	 * 
+	 * THEY WILL BE REMOVED WHEN A GENERAL ERDF IMPORTER IS IMPLEMENTED
+	 * 
+	 */
+	
+	/**
+	 * Loads the imported string into the oryx
+	 * 
+	 * @param {Object} content
+	 */
+/*	loadContent: function(content){
+		
+		var epcs = this.parseToObject( content );
+		
+		epcs = epcs.collect(function(epc){ return {epcData: epc, stencil: ORYX.Core.StencilSet.stencil(epc.type)}})
+		
+		var nodes = epcs.findAll(function(epc){ return epc.stencil.type() == "node" });
+		var edges = epcs.findAll(function(epc){ return epc.stencil.type() == "edge" });
+		
+		nodes = nodes.collect(function(epc){
+									
+			if( !epc.stencil){
+				throw $break;
+			}
+			
+			// Create a new Shape
+			var newShape = 	new ORYX.Core.Node( {'eventHandlerCallback':this.facade.raiseEvent }, epc.stencil );
+											
+			// Add the shape to the canvas
+			this.facade.getCanvas().add(newShape);
+		
+			if( epc.epcData.bounds ){
+				// Set the bounds
+				newShape.bounds.centerMoveTo( epc.epcData.bounds.center )
+			}
+			
+			for (var key in epc.epcData){
+				if (key != "bounds" && key != "id" && key != "type" && key != "outgoing" && key != "parent" && key != "dockers"){
+					newShape.properties['oryx-'+key] = epc.epcData[key];
+				}
+			} 
+			return {epcData: epc.epcData, stencil: epc.stencil, shape: newShape};
+			
+		}.bind(this));
+		
+	
+	
+		edges.each(function(epc){
+									
+			if( !epc.stencil){
+				throw $break;
+			}
+			
+			// Create a new Shape
+			var newShape = new ORYX.Core.Edge({'eventHandlerCallback':this.facade.raiseEvent }, epc.stencil);
+
+			// Add the shape to the canvas
+			this.facade.getCanvas().add(newShape);
+		
+			if( epc.epcData.bounds ){
+				// Set the bounds
+				newShape.bounds.centerMoveTo( epc.epcData.bounds.center )
+			}
+			
+
+			var from0 	= nodes.find(function(node){ return node.epcData.outgoing && node.epcData.outgoing.any(function(out){ return out.slice(1) == epc.epcData.id }) });
+			if (from0)
+				var from = from0.shape;
+			var to0	= nodes.find(function(node){ return epc.epcData.outgoing && node.epcData.id == epc.epcData.outgoing[0].slice(1) });
+			if (to0)
+				var to = to0.shape;
+			// Set the docker
+			if( from ){
+				newShape.dockers.first().setDockedShape( from );
+				newShape.dockers.first().setReferencePoint({x: from.bounds.width() / 2.0, y: from.bounds.height() / 2.0});
+				newShape.dockers.first().update();
+			}
+			if( to ){
+				newShape.dockers.last().setDockedShape( to );
+				newShape.dockers.last().setReferencePoint({x: to.bounds.width() / 2.0, y: to.bounds.height() / 2.0});
+				newShape.dockers.last().update();
+			}
+			
+			for (var key in epc.epcData){
+				if (key != "bounds" && key != "id" && key != "type" && key != "outgoing" && key != "parent" && key != "dockers"){
+					newShape.properties['oryx-'+key] = epc.epcData[key];
+				}
+			}
+			
+		}.bind(this));
+				
+		this.facade.getCanvas().update();
+		
+	},
+	*/
+	/**
+	 * Parsed the given ERDF-String to a Array with the individual
+	 * EPC-Objects
+	 * 
+	 * @param {Object} erdfString
+	 */
+	parseToObject: function ( erdfString ){
+
+		var parser	= new DOMParser();			
+		var doc		= parser.parseFromString( erdfString ,"text/xml");
+
+		var getElementByIdFromDiv = function(id){ return $A(doc.getElementsByTagName('div')).find(function(el){return el.getAttribute("id")== id})}
+
+		// Get the oryx-editor div
+		var editorNode 	= getElementByIdFromDiv('oryxcanvas');
+		editorNode 		= editorNode ? editorNode : getElementByIdFromDiv('oryx-canvas123');
+
+		var hasEPC = editorNode ? $A(editorNode.childNodes).any(function(node){return node.nodeName.toLowerCase() == "a" && node.getAttribute('rel') == 'oryx-stencilset' && node.getAttribute('href').endsWith('epc/epc.json')}) : null;
+
+		if( !hasEPC ){
+			this.throwErrorMessage('Imported model is not an EPC model!');
+			return null
+		}
+
+
+		// Get all ids from the canvas node for rendering
+		var renderNodes = $A(editorNode.childNodes).collect(function(el){ return el.nodeName.toLowerCase() == "a" && el.getAttribute('rel') == 'oryx-render' ? el.getAttribute('href').slice(1) : null}).compact()
+		// Collect all nodes from the ids
+		renderNodes = renderNodes.collect(function(el){return getElementByIdFromDiv(el)});
+
+		// Function for extract all eRDF-Attributes and give them back as an Object
+		var parseAttribute = function(node){
+		    var res = {}
+			// Set the resource id
+			if(node.getAttribute("id")){
+				res["id"] = node.getAttribute("id");
+			}
+			
+			// Set all attributes
+		    $A(node.childNodes).each( function(node){ 
+				if( node.nodeName.toLowerCase() == "span" && node.getAttribute('class')){
+		            var key = node.getAttribute('class').slice(5);
+					res[key] = node.firstChild ? node.firstChild.nodeValue : '';
+		        	if( key == "bounds" ){
+						var ba = $A(res[key].split(",")).collect(function(el){return Number(el)})
+						res[key] = {a:{x:ba[0], y:ba[1]},b:{x:ba[2], y:ba[3]},center:{x:ba[0]+((ba[2]-ba[0])/2),y:ba[1]+((ba[3]-ba[1])/2)}}
+					}
+				} else if( node.nodeName.toLowerCase() == "a" && node.getAttribute('rel')){
+		            var key = node.getAttribute('rel').split("-")[1];
+					if( !res[key] ){
+						res[key] = [];
+					}
+					
+		            res[key].push( node.getAttribute('href') )
+		        }
+		    })
+		    return res
+		}
+
+		// Collect all Attributes out of the Nodes
+		return renderNodes.collect(function(el){return parseAttribute(el)});
+				
+	}	
+	
+});
