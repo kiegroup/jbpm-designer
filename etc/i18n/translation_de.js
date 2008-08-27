e8bac51e538463a323d5a2807ff786f2575c05b4
NicoPeters.NP <NicoPeters.NP@d672c736-503d-0410-a38a-9366997c882b>
2008-08-27 16:23:09 +0000

    added missing javascript file to build script
    added i18n language files
    
    git-svn-id: http://oryx-editor.googlecode.com/svn/trunk@909 d672c736-503d-0410-a38a-9366997c882b

---
+++ etc/i18n/translation_de.js
@@ -0,0 +1,6 @@
+/**
+ * @author nicolas.peters
+ * 
+ * Contains all strings for German language.
+ */
+ORYX.I18N.Language = "de_DE"; //Pattern <ISO language code>_<ISO country code> in lower case!
---
+++ etc/i18n/translation_en_US.js
@@ -0,0 +1,11 @@
+/**
+ * @author nicolas.peters
+ * 
+ * Contains all strings for the default language (en-us).
+ */
+if(!ORYX.I18N) ORYX.I18N = {}; //Only needed in default language file
+
+ORYX.I18N.Language = "en_us"; //Pattern <ISO language code>_<ISO country code> in lower case!
+
+//ORYX.I18N.AddDocker.group = "Docker";
+//ORYX.I18N.AddDocker.add = "Add Docker";
