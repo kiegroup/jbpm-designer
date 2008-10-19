6e73f0e660c7349d7c9342efff9c6eafc24d85c8
bjnwagner <bjnwagner@d672c736-503d-0410-a38a-9366997c882b>
2008-10-19 17:50:07 +0000

    Added missing file from last commit
    git-svn-id: http://oryx-editor.googlecode.com/svn/trunk@1241 d672c736-503d-0410-a38a-9366997c882b

--- poem-jvm/data/database/db_schema.sql
+++ poem-jvm/data/database/db_schema.sql
@@ -1198,6 +1198,30 @@ GRANT ALL ON SCHEMA public TO postgres;
 GRANT ALL ON SCHEMA public TO PUBLIC;
 
 
+-- Table: friend
+
+-- DROP TABLE friend;
+
+CREATE TABLE friend
+(
+  id serial NOT NULL,
+  subject_id integer NOT NULL,
+  friend_id integer NOT NULL,
+  CONSTRAINT friend_pkey PRIMARY KEY (id),
+  CONSTRAINT fkey_friend_identity2 FOREIGN KEY (friend_id)
+      REFERENCES identity (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE CASCADE,
+  CONSTRAINT friends_fkey_identity1 FOREIGN KEY (subject_id)
+      REFERENCES identity (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE CASCADE
+)
+WITH (OIDS=FALSE);
+ALTER TABLE friend OWNER TO poem;
+
+CREATE INDEX friend_identity_idx
+   ON friend USING btree (subject_id, friend_id);
+
+
 --
 -- PostgreSQL database dump complete
 --
