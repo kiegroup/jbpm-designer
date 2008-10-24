102ff2a3803a698ac28a4c649abcb596f8baf92a
bjnwagner <bjnwagner@d672c736-503d-0410-a38a-9366997c882b>
2008-10-24 13:07:59 +0000

    Implemented server-side sorting and filtering for rating.
    git-svn-id: http://oryx-editor.googlecode.com/svn/trunk@1295 d672c736-503d-0410-a38a-9366997c882b

--- poem-jvm/src/java/org/b3mn/poem/handler/RatingHandler.java
+++ poem-jvm/src/java/org/b3mn/poem/handler/RatingHandler.java
@@ -1,13 +1,19 @@
 package org.b3mn.poem.handler;
 
+import java.util.Collection;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.b3mn.poem.Identity;
+import org.b3mn.poem.Persistance;
 import org.b3mn.poem.business.Model;
 import org.b3mn.poem.util.AccessRight;
+import org.b3mn.poem.util.FilterMethod;
 import org.b3mn.poem.util.HandlerWithModelContext;
 import org.b3mn.poem.util.RestrictAccess;
+import org.b3mn.poem.util.SortMethod;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -38,5 +44,40 @@ public class RatingHandler extends HandlerBase {
 		String userScore = request.getParameter("userScore");
 		if (userScore != null) model.setUserScore(subject, Integer.parseInt(userScore));
 		writeRating(model, response, subject, object);
+	}
+	
+	@SuppressWarnings("unchecked")
+	@SortMethod(SortName="rating")	
+	public static List<String> sortByRating(Identity subject) {
+		List<String> results = Persistance.getSession()
+		.createSQLQuery("SELECT access.object_name FROM " 	
+		+ "access LEFT JOIN model_rating ON access.object_id=model_rating.object_id "
+		+ "WHERE access.subject_id=:subject_id  " 
+		+ "GROUP BY access.object_name "
+		+ "ORDER BY avg(model_rating.score) NULLS FIRST")
+		.setInteger("subject_id", subject.getId())
+		.list();
+	
+		Persistance.commit();
+		return results;
+	}
+	
+	@SuppressWarnings({ "unchecked" })
+	@FilterMethod(FilterName="rating")
+	public static Collection<String> filterByRating(Identity subject, String params) {
+		float score = Float.parseFloat(params);
+		
+		List<String> results = Persistance.getSession()
+			.createSQLQuery("SELECT access.object_name FROM access, model_rating " 	
+			+ "WHERE access.subject_id=:subject_id  AND access.object_id=model_rating.object_id " 
+			+ "GROUP BY access.object_name "
+			+ "HAVING avg(model_rating.score) >= :score")
+			.setFloat("score", score)
+			.setInteger("subject_id", subject.getId())
+			.list();
+		
+		Persistance.commit();
+		
+		return results;
 	}
 }
