219768162c705579b9bb704f0ef18b5672e4fb1b
NicoPeters.NP <NicoPeters.NP@d672c736-503d-0410-a38a-9366997c882b>
2009-01-08 10:35:20 +0000

    added some mock objects for junit test
    git-svn-id: http://oryx-editor.googlecode.com/svn/trunk@1696 d672c736-503d-0410-a38a-9366997c882b

---
+++ poem-jvm/test/java/org/b3mn/poem/mock/TestHandlerWithModelContext.java
@@ -0,0 +1,25 @@
+package org.b3mn.poem.mock;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.b3mn.poem.Identity;
+import org.b3mn.poem.business.Model;
+import org.b3mn.poem.handler.HandlerBase;
+import org.b3mn.poem.util.AccessRight;
+import org.b3mn.poem.util.HandlerWithModelContext;
+import org.b3mn.poem.util.RestrictAccess;
+
+@HandlerWithModelContext(uri="/test", filterBrowser=true, accessRestriction=AccessRight.NONE, denyPublicUserAccess=true)
+public class TestHandlerWithModelContext extends HandlerBase {
+
+	@Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response, Identity subject, Identity object) throws Exception {
+		
+	}
+	
+	@Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response, Identity subject, Identity object) throws Exception {
+		
+	}
+}
---
+++ poem-jvm/test/java/org/b3mn/poem/mock/TestHandlerWithoutModelContext.java
@@ -0,0 +1,24 @@
+package org.b3mn.poem.mock;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.b3mn.poem.Identity;
+import org.b3mn.poem.handler.HandlerBase;
+import org.b3mn.poem.util.HandlerWithoutModelContext;
+import org.json.JSONObject;
+
+@HandlerWithoutModelContext(uri="/test2", filterBrowser=true, denyPublicUserAccess=true)
+public class TestHandlerWithoutModelContext extends HandlerBase {
+
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response, Identity subject, Identity object) throws Exception {
+		
+	}
+	
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response, Identity subject, Identity object) throws Exception {
+	
+	}
+	
+}
