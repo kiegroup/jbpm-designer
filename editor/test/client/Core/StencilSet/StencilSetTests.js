26dc60b867532727a1cd9566ddeb75818f8bd89c
sven.wagnerboysen <sven.wagnerboysen@d672c736-503d-0410-a38a-9366997c882b>
2009-02-12 23:30:54 +0000

    tests for static stencil set methods in stencilsets.js
    git-svn-id: http://oryx-editor.googlecode.com/svn/trunk@1789 d672c736-503d-0410-a38a-9366997c882b

--- editor/test/client/Core/StencilSet/StencilSetTests.js
+++ editor/test/client/Core/StencilSet/StencilSetTests.js
@@ -1,12 +1,79 @@
 
-var stencilSet1 = new ORYX.Core.StencilSet.StencilSet("../../data/stencilsets/bpmn1.1/bpmn1.1.json");
-var stencilSetInvalid = new ORYX.Core.StencilSet.StencilSet("../../data/stencilsets/inval/invalid.json");
+// Mocks
 
-function setUpPage() {
-	setTimeout("setUpPageStatus = 'complete'", 6000);
+// mocking Ajax Object
+Ajax = {}
+
+// Stubs
+
+// stubing Ajax.request method
+Ajax.Request = function(source, options) {
+	var response = {}
+	response.responseText = Object.toJSON(testStencilSetJSON)
+	options.onSuccess(response)
+} 
+
+
+//var testStencilSet = new ORYX.Core.StencilSet.StencilSet("./fixtures/StencilSet/testStencilSet.json")
+
+function setUp() {
+	testStencilSetURL = "../../../test/client/Core/fixtures/StencilSet/testStencilSet.json"
+	testCorruptedStencilSetURL = "../../../test/client/Core/fixtures/StencilSet/testCorruptedStencilSet.json"
 }
 
-function testConstruct() {
-	alert(stencilSetInvalid);
-	assert(stencilSet1.errornous!=true);
+/**
+ * Tests, if missing parameters are handled.
+ */
+function testFailForMissingSourceParameter() {
+	try {
+		var testStencilSet = new ORYX.Core.StencilSet.StencilSet()
+		fail("Test case should fail yet, because of missing source parameter")
+	} catch(e) {
+		if((e instanceof JsUnitException)) {
+			throw e
+		}
+	}
+}
+
+/**
+ * Tests, if NoSuchFile Error is handled.
+ */
+function testFailForNoSuchFile() {
+	try {
+		var testStencilSet = new ORYX.Core.StencilSet.StencilSet("./badDir/zzz.json")
+		
+		fail("Test case should fail yet, because of NoSuchFile Error.")
+	} catch(e) {
+		if((e instanceof JsUnitException)) {
+			throw e
+		}
+	}
+}
+
+/**
+ * Tests, if corrupted JSON-Format is handled.
+ */
+function testFailForCorruptedJSON() {
+	try {
+		var testStencilSet = new ORYX.Core.StencilSet.StencilSet(testCorruptedStencilSetURL)
+		//alert(testStencilSet)
+		fail("Test case should fail yet, because of corrupted stencil set json format.")
+	} catch(e) {
+		if((e instanceof JsUnitException)) {
+			throw e
+		}
+	}
+}
+
+function testConstructor() {
+	try {
+		alert("hier ss")
+		alert(testStencilSetJSON)
+		var testStencilSet = new ORYX.Core.StencilSet.StencilSet("./test/url.json")
+	} catch (e) {
+		if(!(e instanceof JsUnitException)) {
+			throw e
+		}
+	}
+	assertFalse(testStencilSet.errornous)
 }
