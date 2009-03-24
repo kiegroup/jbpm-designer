60d0a8dba2ded2e5e1e02d56445d23c4ad3962b5
falko.menge <falko.menge@d672c736-503d-0410-a38a-9366997c882b>
2009-03-24 09:41:28 +0000

    XForms Editor: fixed content type in XForms export
    git-svn-id: http://oryx-editor.googlecode.com/svn/trunk@1974 d672c736-503d-0410-a38a-9366997c882b

--- editor/server/src/org/oryxeditor/server/XFormsExportServlet.java
+++ editor/server/src/org/oryxeditor/server/XFormsExportServlet.java
@@ -13,15 +13,16 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.w3c.dom.Document;
-/*import org.w3c.dom.DOMImplementation;
-import org.w3c.dom.bootstrap.DOMImplementationRegistry;
-import org.w3c.dom.ls.DOMImplementationLS;
-import org.w3c.dom.ls.LSOutput;
-import org.w3c.dom.ls.LSSerializer;*/
+/*import org.w3c.dom.DOMImplementation;                                                                                           
+import org.w3c.dom.bootstrap.DOMImplementationRegistry;                                                                           
+import org.w3c.dom.ls.DOMImplementationLS;                                                                                        
+import org.w3c.dom.ls.LSOutput;                                                                                                   
+import org.w3c.dom.ls.LSSerializer;*/                                                                                             
 
+import com.sun.org.apache.xml.internal.serialize.DOMSerializer;
+import com.sun.org.apache.xml.internal.serialize.Method;
 import com.sun.org.apache.xml.internal.serialize.OutputFormat;
 import com.sun.org.apache.xml.internal.serialize.XMLSerializer;
-import com.sun.org.apache.xml.internal.serialize.DOMSerializer;
 
 import de.hpi.xforms.XForm;
 import de.hpi.xforms.rdf.XFormsRDFImporter;
@@ -38,10 +39,15 @@ public class XFormsExportServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 
-		res.setContentType("text/xhtml");
+		//TODO: examination of the HTTP Accept header (see http://www.w3.org/TR/xhtml-media-types/#media-types)
+		res.setContentType("application/xhtml+xml");
 		
 		String rdf = req.getParameter("data");
 		String cssUrl = req.getParameter("css");
+
+		if (cssUrl == null) {
+			cssUrl = "/oryx/css/xforms_default.css";
+		}
 		
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 		factory.setNamespaceAware(true);
@@ -57,7 +63,8 @@ public class XFormsExportServlet extends HttpServlet {
 	
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		
-		res.setContentType("text/xhtml");
+		//TODO: examination of the HTTP Accept header (see http://www.w3.org/TR/xhtml-media-types/#media-types)
+		res.setContentType("application/xhtml+xml");
 		
 		String path = req.getParameter("path");
 		String cssUrl = req.getParameter("css");
@@ -65,7 +72,7 @@ public class XFormsExportServlet extends HttpServlet {
 		if(path==null)
 			return;
 		if(cssUrl==null)
-			cssUrl = "";
+			cssUrl = "/oryx/css/xforms_default.css";
 		
 		if(path.endsWith("/self")) path = path.substring(0, path.indexOf("/self"));
 		
@@ -113,7 +120,7 @@ public class XFormsExportServlet extends HttpServlet {
 			format.setIndenting(true);
 			format.setPreserveSpace(true);
 			format.setLineSeparator(System.getProperty("line.separator"));
-			format.setMethod("text/xml");
+			format.setMethod(Method.XHTML);
 			
 			// TODO: newlines in serialize output (weird it doesn't work)
 			
