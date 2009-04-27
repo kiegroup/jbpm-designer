dac2d0258af3ee9c4055ed0aee51f689ddde7580
stefan.krumnow@student.hpi.uni-potsdam.de <stefan.krumnow@student.hpi.uni-potsdam.de@d672c736-503d-0410-a38a-9366997c882b>
2009-04-27 09:39:36 +0000

    Updating jdpl plugin..
    git-svn-id: http://oryx-editor.googlecode.com/svn/trunk@2137 d672c736-503d-0410-a38a-9366997c882b

--- editor/client/scripts/Plugins/jpdlSupport.js
+++ editor/client/scripts/Plugins/jpdlSupport.js
@@ -30,6 +30,9 @@ ORYX.Plugins.JPDLSupport = Clazz.extend({
 	
 	jPDLImporterUrl: '/backend/poem/new_jpdl',
 	jPDLExporterUrlSuffix: '/jpdl',
+	
+	stencilSetExtensionSuffix: '/jbpm#',
+	stencilSetUrlSuffix: '/bpmn1.1/bpmn1.1.json',
 
 	/**
 	 * constructor method
@@ -47,7 +50,9 @@ ORYX.Plugins.JPDLSupport = Clazz.extend({
 			'description': 		ORYX.I18N.jPDLSupport.expDesc,
 			'index': 			1,
 			'minShape': 		0,
-			'maxShape': 		0
+			'maxShape': 		0,
+			'maxShape': 		0,
+			'isEnabled': 		this.isStencilSetExtensionLoaded.bind(this)
 		});
 					
 		this.facade.offer({
@@ -58,13 +63,27 @@ ORYX.Plugins.JPDLSupport = Clazz.extend({
 			'description': 		ORYX.I18N.jPDLSupport.impDesc,
 			'index': 			2,
 			'minShape': 		0,
-			'maxShape': 		0
+			'maxShape': 		0,
+			'isEnabled': 		this.isStencilSetExtensionLoaded.bind(this)
 		});
 
+	},
+	
+	isStencilSetExtensionLoaded: function() {
+		return this.facade.getStencilSets().values().any(
+			function(ss){ 
+				return ss.extensions().keys().any(
+					function(extensionKey) {
+						return extensionKey.endsWith(this.stencilSetExtensionSuffix);
+					}.bind(this)
+				);
+			}.bind(this)
+		);
 	},
 
 	
 	importJPDL: function(){
+		// TODO: Load extension on import..
 		this._showImportDialog();
 	},		
 
@@ -73,7 +92,7 @@ ORYX.Plugins.JPDLSupport = Clazz.extend({
 		var jpdlLoc ;
 		if ( loc.length > 4 && loc.substring(loc.length - 5) == "/self" ) {
 			jpdlLoc = loc.substring(0, loc.length - 5) + this.jPDLExporterUrlSuffix;
-		} else {
+		} else {		// test data:
 			alert("TODO: Integrate existing export with new models.. ");
 			return ;
 		}
@@ -118,11 +137,12 @@ ORYX.Plugins.JPDLSupport = Clazz.extend({
 	
 	_loadJSON: function( jsonString ){
 		
-		// test data:
-		//jsonString = '{"resourceId":"oryx-canvas123","properties":{"id":"","name":"","version":"","author":"","language":"English","expressionlanguage":"","querylanguage":"","creationdate":"","modificationdate":"","pools":"","documentation":""},"stencil":{"id":"BPMNDiagram"},"childShapes":[{"resourceId":"oryx_B9194F2B-68EE-4AC1-B9A3-AB197A3E5483","properties":{"id":"","categories":"","documentation":"","name":"","assignments":"","pool":"","lanes":"","eventtype":"Start","trigger":"None","bgcolor":"#ffffff"},"stencil":{"id":"StartEvent"},"childShapes":[],"outgoing":[{"resourceId":"oryx_937581EC-B462-4A79-A112-3B5E922ABE5F"}],"bounds":{"lowerRight":{"x":197,"y":148},"upperLeft":{"x":167,"y":118}},"dockers":[]},{"resourceId":"oryx_7EF6A824-175A-49C5-8AF2-63C705C94E3C","properties":{"id":"","categories":"","documentation":"","name":"Test","assignments":"","pool":"","lanes":"","activitytype":"Task","status":"None","performers":"","properties":"","inputsets":"","inputs":"","outputsets":"","outputs":"","iorules":"","startquantity":1,"completionquantity":1,"looptype":"None","loopcondition":"","loopcounter":1,"loopmaximum":1,"testtime":"After","mi_condition":"","mi_ordering":"Sequential","mi_flowcondition":"All","complexmi_condition":"","iscompensation":"","tasktype":"None","inmessage":"","outmessage":"","implementation":"Webservice","messageref":"","instantiate":"","script":"","taskref":"","bgcolor":"#ffffcc"},"stencil":{"id":"Task"},"childShapes":[],"outgoing":[{"resourceId":"oryx_202FB34E-3C30-4BBC-B98A-4C3DDF0A595A"}],"bounds":{"lowerRight":{"x":374,"y":166},"upperLeft":{"x":274,"y":86}},"dockers":[]},{"resourceId":"oryx_937581EC-B462-4A79-A112-3B5E922ABE5F","properties":{"id":"","categories":"","documentation":"","name":"","sourceref":"","targetref":"","conditiontype":"None","conditionexpression":"","quantity":1,"showdiamondmarker":"false"},"stencil":{"id":"SequenceFlow"},"childShapes":[],"outgoing":[{"resourceId":"oryx_7EF6A824-175A-49C5-8AF2-63C705C94E3C"}],"bounds":{"lowerRight":{"x":273.0793378267027,"y":132.23982651258393},"upperLeft":{"x":197.42066217329727,"y":128.51017348741607}},"dockers":[{"x":15,"y":15},{"x":50,"y":40}]},{"resourceId":"oryx_0BBA69E4-B6AC-4CCC-9AC5-0D6583FBD7FC","properties":{"id":"","categories":"","documentation":"","name":"","assignments":"","pool":"","lanes":"","eventtype":"End","result":"None","bgcolor":"#ffffff"},"stencil":{"id":"EndEvent"},"childShapes":[],"outgoing":[],"bounds":{"lowerRight":{"x":490,"y":156},"upperLeft":{"x":462,"y":128}},"dockers":[]},{"resourceId":"oryx_202FB34E-3C30-4BBC-B98A-4C3DDF0A595A","properties":{"id":"","categories":"","documentation":"","name":"","sourceref":"","targetref":"","conditiontype":"None","conditionexpression":"","quantity":1,"showdiamondmarker":"false"},"stencil":{"id":"SequenceFlow"},"childShapes":[],"outgoing":[{"resourceId":"oryx_0BBA69E4-B6AC-4CCC-9AC5-0D6583FBD7FC"}],"bounds":{"lowerRight":{"x":466.59837667841475,"y":131.50516466427695},"upperLeft":{"x":374.3501305552209,"y":94}},"dockers":[{"x":50,"y":40},{"x":433,"y":94},{"x":14,"y":14}]}],"bounds":{"lowerRight":{"x":1485,"y":1050},"upperLeft":{"x":0,"y":0}},"stencilset":{"url":"/oryx/stencilsets/bpmn1.1/bpmn1.1.json"},"ssextensions":[]}';
 		if (jsonString) {
 			var jsonObj = jsonString.evalJSON();
 			if( jsonObj && this._hasStencilset(jsonObj) ) {
+				if ( ! this.isStencilSetExtensionLoaded() ) {
+					// TODO: Load extension on import..
+				}
 				this.facade.importJSON(jsonString);
 			}
 		}
@@ -130,11 +150,14 @@ ORYX.Plugins.JPDLSupport = Clazz.extend({
 	},
 	
 	_hasStencilset: function( jsonObj ){
-		// TODO: Implement real check.
-		return true;
+		return jsonObj.properties.ssextension.endsWith(this.stencilSetExtensionSuffix) && jsonObj.stencilset.url.endsWith(this.stencilSetUrlSuffix);
 	},
 	
 
+	/**
+	 * Opens an export window / tab.
+	 * 
+	 */
 	_openExportWindow: function( url ){
 		// TODO: Errors in jpdl document should be handled in Oryx UI
 		window.open( url );
