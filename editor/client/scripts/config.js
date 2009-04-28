018f5a94d24f7b47ffda15c2baa0a1faa19ceb56
sven.wagnerboysen <sven.wagnerboysen@d672c736-503d-0410-a38a-9366997c882b>
2009-04-28 12:43:54 +0000

    corrected editor root path
    git-svn-id: http://oryx-editor.googlecode.com/svn/trunk@2149 d672c736-503d-0410-a38a-9366997c882b

--- editor/client/scripts/config.js
+++ editor/client/scripts/config.js
@@ -6,7 +6,7 @@ if(!ORYX) var ORYX = {};
 if(!ORYX.CONFIG) ORYX.CONFIG = {};
 
 //This is usually the name of the war file!
-ORYX.CONFIG.ROOT_PATH =					"./";
+ORYX.CONFIG.ROOT_PATH =					"/oryx/";
 	
 ORYX.CONFIG.VERSION_URL =				ORYX.CONFIG.ROOT_PATH + "VERSION";
 ORYX.CONFIG.LICENSE_URL =				ORYX.CONFIG.ROOT_PATH + "LICENSE";
