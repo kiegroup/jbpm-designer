ad2b8e3750e380765c9514614a65b3b432b2e39c
jan-felix.schwarz@student.hpi.uni-potsdam.de <jan-felix.schwarz@student.hpi.uni-potsdam.de@d672c736-503d-0410-a38a-9366997c882b>
2009-05-25 10:58:32 +0000

    updated MOVI API doc
    git-svn-id: http://oryx-editor.googlecode.com/svn/trunk@2306 d672c736-503d-0410-a38a-9366997c882b

--- poem-jvm/src/javascript/movi/doc/classmap.js
+++ poem-jvm/src/javascript/movi/doc/classmap.js
@@ -1,4 +1,4 @@
-YAHOO.env.classMap = {"MOVI.model.Edge": "movi", "MOVI.model.Canvas": "movi", "MOVI.model.Shape": "movi", "MOVI.util.Marker": "movi", "MOVI.widget.ModelNavigator": "movi", "MOVI.widget.ModelViewer": "movi", "MOVI.model.Node": "movi", "MOVI.stencilset.Stencilset": "movi", "MOVI.util.Annotation": "movi", "MOVI.stencilset.Stencil": "movi", "MOVI.util.ShapeSelect": "movi"};
+YAHOO.env.classMap = {"MOVI.model.Edge": "movi", "MOVI.widget.Toolbar": "movi", "MOVI.model.Canvas": "movi", "MOVI.model.Shape": "movi", "MOVI.util.Marker": "movi", "MOVI.widget.FullscreenViewer": "movi", "MOVI.widget.ModelNavigator": "movi", "MOVI.widget.ModelViewer": "movi", "MOVI.model.Node": "movi", "MOVI.stencilset.Stencilset": "movi", "MOVI.widget.ZoomSlider": "movi", "MOVI.util.Annotation": "movi", "MOVI.stencilset.Stencil": "movi", "MOVI.util.ShapeSelect": "movi"};
 
 YAHOO.env.resolveClass = function(className) {
     var a=className.split('.'), ns=YAHOO.env.classMap;
