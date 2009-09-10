e0447b23ad97be58a4473e900517110711be34bb
jan-felix.schwarz@student.hpi.uni-potsdam.de <jan-felix.schwarz@student.hpi.uni-potsdam.de@d672c736-503d-0410-a38a-9366997c882b>
2009-09-10 09:38:16 +0000

    extended the stencil set jsonp handler to allow requests for stencil set extensions as well
    git-svn-id: http://oryx-editor.googlecode.com/svn/trunk@2656 d672c736-503d-0410-a38a-9366997c882b

--- editor/server/src/org/oryxeditor/server/StencilSetJSONPServlet.java
+++ editor/server/src/org/oryxeditor/server/StencilSetJSONPServlet.java
@@ -11,6 +11,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 
 /**
  * 
@@ -26,46 +29,78 @@ public class StencilSetJSONPServlet extends HttpServlet {
 		res.setContentType("application/json");
 		
 		String resource = req.getParameter("resource");
+		String type = req.getParameter("type");
 		String jsonp = req.getParameter("jsonp");
 		String embedSvg = req.getParameter("embedsvg");
 
 		try {
 			PrintWriter out = res.getWriter();
 			
-			resource.replace("..", ""); // don't allow to leave stencilset dir
 			
-			if(resource.startsWith("/"))
-				resource = resource.substring(1);
-			
-			if(resource.startsWith("stencilsets/")) // ignore stencilset/ at beginning of path reference
-				resource = resource.substring(12);
-
-			File jsonFile;
-			if(embedSvg!=null && embedSvg.equals("true")) { // SVG embedding
-				jsonFile = new File(getServletContext().getRealPath("/stencilsets/" + resource));
-			} else { // no SVG embedding (default)
-				// try to find stencilset nosvg representation
-				int pIdx = resource.lastIndexOf('.');
-				jsonFile = new File(getServletContext().getRealPath("/stencilsets/" + 
-						resource.substring(0, pIdx) + "-nosvg" + resource.substring(pIdx)));
-				if(!jsonFile.exists())
-					jsonFile = new File(getServletContext().getRealPath("/stencilsets/" + resource));
-			}
-			
-			if(!jsonFile.exists()) {
-				if(jsonp!=null) out.write(jsonp + "({ \"error\":\"resource not found\" })");
-				else out.write("{ \"error\":\"resource not found\" }");
-			}
+			// also allow to request stencil set extensions (specified by namespace)
+			if(type!=null && type.equals("ssextension")) {
 				
-			if(jsonp!=null) out.append(jsonp + "(");
-			
-			BufferedReader reader = new BufferedReader(new FileReader(jsonFile));
-			String line = null;
-			while (( line = reader.readLine()) != null){
-		          out.append(line);
-		          out.append(System.getProperty("line.separator"));
-		    }
-			if(jsonp!=null) out.append(");");
+				File extensionsFile = new File(getServletContext().getRealPath("/stencilsets/extensions/extensions.json"));
+				BufferedReader reader = new BufferedReader(new FileReader(extensionsFile));
+				String line = null; String jsonString = "";
+				while (( line = reader.readLine()) != null){
+			          jsonString += line;
+			    }
+				JSONObject jsonObj = new JSONObject(jsonString);
+				JSONArray extArr = jsonObj.getJSONArray("extensions");
+				for(int i=0; i<extArr.length(); i++) {
+					if(extArr.getJSONObject(i).getString("namespace").equals(resource)) {
+						String definition = extArr.getJSONObject(i).getString("definition");
+						File jsonFile = new File(getServletContext().getRealPath("/stencilsets/extensions/"+definition));
+						if(jsonp!=null) out.append(jsonp + "(");
+						BufferedReader reader2 = new BufferedReader(new FileReader(jsonFile));
+						String line2 = null;
+						while (( line2 = reader2.readLine()) != null){
+					          out.append(line2);
+					          out.append(System.getProperty("line.separator"));
+					    }
+						if(jsonp!=null) out.append(");");
+					}
+				}
+				
+			} else { // standard stencil set requested (specified by path)
+				
+				resource.replace("..", ""); // don't allow to leave stencilset dir
+				
+				if(resource.startsWith("/"))
+					resource = resource.substring(1);
+				
+				if(resource.startsWith("stencilsets/")) // ignore stencilset/ at beginning of path reference
+					resource = resource.substring(12);
+
+				File jsonFile;
+				if(embedSvg!=null && embedSvg.equals("true")) { // SVG embedding
+					jsonFile = new File(getServletContext().getRealPath("/stencilsets/" + resource));
+				} else { // no SVG embedding (default)
+					// try to find stencilset nosvg representation
+					int pIdx = resource.lastIndexOf('.');
+					jsonFile = new File(getServletContext().getRealPath("/stencilsets/" + 
+							resource.substring(0, pIdx) + "-nosvg" + resource.substring(pIdx)));
+					if(!jsonFile.exists())
+						jsonFile = new File(getServletContext().getRealPath("/stencilsets/" + resource));
+				}
+				
+				if(!jsonFile.exists()) {
+					if(jsonp!=null) out.write(jsonp + "({ \"error\":\"resource not found\" })");
+					else out.write("{ \"error\":\"resource not found\" }");
+				}
+					
+				if(jsonp!=null) out.append(jsonp + "(");
+				
+				BufferedReader reader = new BufferedReader(new FileReader(jsonFile));
+				String line = null;
+				while (( line = reader.readLine()) != null){
+			          out.append(line);
+			          out.append(System.getProperty("line.separator"));
+			    }
+				if(jsonp!=null) out.append(");");
+				
+			}
 			
 		} catch (Exception e) {
 			e.printStackTrace();
