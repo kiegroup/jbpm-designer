dee1c59cb1c50535abde60313f014dd77cf41056
ahmad.gaafar@gmail.com <ahmad.gaafar@gmail.com@d672c736-503d-0410-a38a-9366997c882b>
2010-01-21 09:06:53 +0000

    Updated Query Evaluation Servlet for BPMN-Q
    git-svn-id: http://oryx-editor.googlecode.com/svn/trunk@3143 d672c736-503d-0410-a38a-9366997c882b

--- editor/server/src/org/oryxeditor/server/QueryEvalServlet.java
+++ editor/server/src/org/oryxeditor/server/QueryEvalServlet.java
@@ -26,6 +26,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -41,7 +42,7 @@ import com.bpmnq.AbstractQueryProcessor;
 import com.bpmnq.FileFormatException;
 import com.bpmnq.GraphBuilder;
 import com.bpmnq.Match;
-import com.bpmnq.MemoryQueryProcessor;
+
 import com.bpmnq.OryxMemoryQueryProcessor;
 import com.bpmnq.ProcessGraph;
 import com.bpmnq.QueryGraph;
@@ -55,7 +56,7 @@ import com.bpmnq.compliancechecker.TemporalQueryGraph;
 
 public class QueryEvalServlet extends HttpServlet {
     private static final long serialVersionUID = -7946509291423453168L;
-    private static final boolean useDataBaseConnection = true;
+    private static final boolean useDataBaseConnection = false;
     private Logger log = Logger.getLogger(this.getClass());
     private Map<QueryGraph,String> queryMatches = new HashMap<QueryGraph,String>(); 
     /* (non-Javadoc)
@@ -76,6 +77,7 @@ public class QueryEvalServlet extends HttpServlet {
             Utilities util = Utilities.getInstance();
             if (useDataBaseConnection && !Utilities.isConnectionOpen()) {
                 Utilities.openConnection();
+                System.out.println(" +++++++++++++++ DB Connection has been opened ++++++++++++");
             }
 
         } catch (Exception ex) {
@@ -129,8 +131,12 @@ public class QueryEvalServlet extends HttpServlet {
 //        		respWriter.println("<UserDirectory>"+System.getProperty("user.dir")+"</UserDirectory>");
         		System.out.println("########################################## PROCESS COMPLIANCE QUERY #################################");
         		TemporalQueryGraph tqry = query.getTemporalQueryGraph();
+        		System.out.println("%%%%%%%%%%%%%%%%%%%% Temporal Query Graph");
+        		tqry.print(System.out);
+        		System.out.println("%%%%%%%%%%%%%%%%%%%% Temporal Query Graph");
         		ModelChecker mc = new ModelChecker(tqry,respWriter);
         		mc.queryProc = new OryxMemoryQueryProcessor(respWriter);
+        		
         		List<String> mdls =null;
         		try{
         		   mdls = qProcessor.findRelevantProcessModels(query);
@@ -142,10 +148,12 @@ public class QueryEvalServlet extends HttpServlet {
         		mc.queryProc.procCmd = ProcessorCommand.ComplianceQuery;
         		respWriter.println("<query-result>");
 //        		System.out.println("<query-result>");
-        		
+        		boolean generateAntiPattern = false;
+        		List<String> compliantModels = new ArrayList<String>();
+        		List<ProcessGraph> nonCompliantProcesses = new ArrayList<ProcessGraph>();
         		for (int i = 0; i < mdls.size();i++)
         		{
-        			boolean generateAntiPattern = false;
+        			
         			try
         			{
         				System.out.println("############# Checking model "+mdls.get(i));
@@ -155,83 +163,96 @@ public class QueryEvalServlet extends HttpServlet {
         				{
         					generateAntiPattern = true;
         					System.out.println("Query didnt find a match -> Does not comply :( ...");
+        					nonCompliantProcesses.add(mc.queryProc.getInspectedProcess());
 
         				}
         				else if (result == ModelChecker.RET_NET_DOESNT_COMPLY)
         				{
         					generateAntiPattern = true;
         					System.out.println("Does not Comply :( ...");
-
+        					nonCompliantProcesses.add(mc.queryProc.getInspectedProcess());
         				}
-        				if (generateAntiPattern)
+        				else if (result == ModelChecker.RET_NET_COMPLIES)
+        					
         				{
-        					ComplianceViolationExplanator cve = new ComplianceViolationExplanator(mdls.get(i));
-        					
-        					List<QueryGraph> antiPatterns =  cve.explainViolation(tqry);// tqry.generateAntiPatternQueries();
-        					
-        					for (QueryGraph q : antiPatterns)
-        					{
-        						System.out.println("########################################## ANTI PATTERN QUERY #################################");
-        						q.print(System.out);
-        						System.out.println("########################################## ANTI PATTERN QUERY #################################");
-        						// this has to be changed
-        						List<String> mdls2 = qProcessor.findRelevantProcessModels(q);
-        						
-        						for (String s : mdls2)
-        						{
-        							boolean insert = false;
-        							ProcessGraph matchAntiPattern=null;
-        							QueryGraph cln = (QueryGraph) q.clone();
-        							matchAntiPattern = qProcessor.runQueryAgainstModel(cln, s);
-        							if(matchAntiPattern.nodes.size() > 0)
-        							{
-        								
-        								boolean qFound= false;
-        								for (QueryGraph qq : queryMatches.keySet())
-        								{
-        									if (qq.getSignature().containsAll(q.getSignature()))
-        									{
-        										qFound = true;
-        										String mdlls = queryMatches.get(qq);
-        										if (!mdlls.contains(s))
-        										{
-        											mdlls +=","+s;
-        											queryMatches.put(qq, mdlls);
-        											insert = true;
-        										}
-        									}
-        								}
-        								if (!qFound)
-        								{
-        									insert = true;
-        									queryMatches.put(q, s);
-        								}
-        								System.out.println("############################### Insert value is "+insert);
-        								if (insert)
-        								{
-        									matchAntiPattern.modelURI = s;
-        									matchAntiPattern.exportXML(respWriter,"<match>antipattern</match>\n<diagnosis>violation scenario</diagnosis>");
-        									matchAntiPattern.print(System.out);
-        									System.out.println("<match>antipattern</match>\n<diagnosis>violation scenario</diagnosis>");
-        								}
-        								
-        							}
-        							else
-        							{
-        								System.out.println("Anti Pattern Query didnt find a match");
-        							}
-        						}
-        					}
-
+        					compliantModels.add(mdls.get(i));
         				}
         			}
         			catch(Exception e)
         			{
         				respWriter.println("<Exception>"+e.getMessage()+"</Exception>");
         			}
-
         		}
-        		
+        		if (generateAntiPattern)
+        		{
+//      			ComplianceViolationExplanator cve = new ComplianceViolationExplanator(mdls.get(i));
+        			
+        			for (ProcessGraph tmp : nonCompliantProcesses)
+        			{
+        				System.out.println("%%%%%%%%%%%%%%%%%%%% Non Compliant Process Model "+tmp.modelURI);
+        				mc.getProcessGraph().print(System.out);
+        				System.out.println("%%%%%%%%%%%%%%%%%%%% ");
+        				ComplianceViolationExplanator cve = new ComplianceViolationExplanator(tmp);
+        				List<QueryGraph> antiPatterns =  cve.explainViolation(tqry);//tqry.generateAntiPatternQueries();// cve.explainViolation(tqry);
+
+        				for (QueryGraph q : antiPatterns)
+        				{
+        					System.out.println("########################################## ANTI PATTERN QUERY #################################");
+        					q.print(System.out);
+        					System.out.println("########################################## ANTI PATTERN QUERY #################################");
+        					// this has to be changed
+        					List<String> mdls2 = qProcessor.findRelevantProcessModels(q);
+
+        					for (String s : mdls2)
+        					{
+        						if (compliantModels.contains(s))
+        							continue;
+
+        						boolean insert = false;
+        						ProcessGraph matchAntiPattern=null;
+        						QueryGraph cln = (QueryGraph) q.clone();
+        						matchAntiPattern = qProcessor.runQueryAgainstModel(cln, s);
+        						if(matchAntiPattern.nodes.size() > 0)
+        						{
+
+//      							boolean qFound= false;
+//      							for (QueryGraph qq : queryMatches.keySet())
+//      							{
+//      							if (qq.getSignature().containsAll(q.getSignature()))
+//      							{
+//      							qFound = true;
+//      							String mdlls = queryMatches.get(qq);
+//      							if (!mdlls.contains(s))
+//      							{
+//      							mdlls +=","+s;
+//      							queryMatches.put(qq, mdlls);
+//      							insert = true;
+//      							}
+//      							}
+//      							}
+//      							if (!qFound)
+//      							{
+//      							insert = true;
+//      							queryMatches.put(q, s);
+//      							}
+//      							System.out.println("############################### Insert value is "+insert);
+//      							if (insert)
+//      							{
+        							matchAntiPattern.modelURI = s;
+        							matchAntiPattern.exportXML(respWriter,"<match>antipattern</match>\n<diagnosis>violation scenario</diagnosis>");
+//      							matchAntiPattern.print(System.out);
+        							System.out.println("<match>antipattern</match>\n<diagnosis>violation scenario</diagnosis>");
+//      							}
+
+        						}
+        						else
+        						{
+        							System.out.println("Anti Pattern Query didnt find a match");
+        						}
+        					}
+        				}
+        			}
+        		}
         		respWriter.println("</query-result>");
         		System.out.println("########################################## PROCESS COMPLIANCE QUERY #################################");
         	}
@@ -262,7 +283,7 @@ public class QueryEvalServlet extends HttpServlet {
             	}
             	if (generateAntiPattern)
             	{
-            		ComplianceViolationExplanator cve = new ComplianceViolationExplanator(modelID);
+            		ComplianceViolationExplanator cve = new ComplianceViolationExplanator(mc.getProcessGraph());
 					
 					List<QueryGraph> antiPatterns =  cve.explainViolation(tqry);// tqry.generateAntiPatternQueries();
             		ProcessGraph matchAntiPattern=null;
