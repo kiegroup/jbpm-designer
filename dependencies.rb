e50497a2c885a97fff6bd0da5fe99bf69d56ee36
Antoine Toulme <antoine@lunar-ocean.com>
2010-11-10 14:54:36 -0800

    add yui dependency

--- dependencies.rb
+++ dependencies.rb
@@ -32,4 +32,4 @@ WAR_LIBS = ["org.json:json:jar:20090211", "jbpt:jbpt:jar:0.1.0", "commons-collec
   "org.slf4j:log4j-over-slf4j:jar:1.6.0", "rhino:js:jar:1.7R2", "javax.mail:mail:jar:1.4.1",
   "batik:batik-transcoder:jar:1.6", "xalan:xalan:jar:2.7.1", "xerces:xercesImpl:jar:2.9.1",
   "org.codehaus.jackson:jackson-core-asl:jar:1.5.6", "org.codehaus.jackson:jackson-mapper-asl:jar:1.5.6",
-  "org.eclipse:osgi:jar:3.5.0.v20090520"] | BPMN2_LIBS
+  "org.eclipse:osgi:jar:3.5.0.v20090520", YUICOMPRESSOR] | BPMN2_LIBS
