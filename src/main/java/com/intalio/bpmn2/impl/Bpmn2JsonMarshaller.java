b5ac7f66dfb8710ac8df37fe0b6f494f89f11df4
Tihomir Surdilovic <tsurdilo@redhat.com>
2011-02-19 00:24:21 -0500

    removed println statement

--- src/main/java/com/intalio/bpmn2/impl/Bpmn2JsonMarshaller.java
+++ src/main/java/com/intalio/bpmn2/impl/Bpmn2JsonMarshaller.java
@@ -201,7 +201,6 @@ public class Bpmn2JsonMarshaller {
 	            if (rootElement instanceof Process) {
 	                // have to wait for process node to finish properties and stencil marshalling
 	                props.put("executable", ((Process) rootElement).isIsExecutable() + "");
-	                System.out.println("Setting id to : " + ((Process) rootElement).getId());
 	                props.put("id", ((Process) rootElement).getId());
 	                
 	                // packageName is a jbpm-specific extension attribute
