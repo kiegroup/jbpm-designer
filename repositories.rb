dc15382c8425f4d51a5e57ec3f4464a521fc952e
Douglas Palmer <dpalmer@redhat.com>
2011-12-09 08:50:08 +0000

    First pass at maven build

--- repositories.rb
+++
@@ -1,6 +0,0 @@
-
-repositories.remote = ["http://repo1.maven.org/maven2", "https://repository.jboss.org/nexus/content/repositories/central", "https://repository.jboss.org/nexus/content/repositories/snapshots", "https://repository.jboss.org/nexus/content/repositories/thirdparty-uploads", "http://release.intalio.com/m2repo", "http://www.intalio.org/public/maven2", "http://dist.codehaus.org/mule/dependencies/maven2", "http://repository.jboss.com/maven2", "http://repo1.maven.org/maven2", "http://mirrors.ibiblio.org/pub/mirrors/maven2" ]
-
-repositories.release_to[:username] ||= "release"
-repositories.release_to[:url] ||= "sftp://www.intalio.org/var/www-org/public/maven2"
-repositories.release_to[:permissions] ||= 0664
