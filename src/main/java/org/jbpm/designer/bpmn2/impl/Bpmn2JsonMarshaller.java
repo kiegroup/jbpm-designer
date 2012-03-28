3c552cc1e5f41684c6f3e1de469503c684ddd83e
Tihomir Surdilovic <tsurdilo@redhat.com>
2012-03-28 18:42:53 -0400

    support for skippable property for user tasks

--- src/main/java/org/jbpm/designer/bpmn2/impl/Bpmn2JsonMarshaller.java
+++ src/main/java/org/jbpm/designer/bpmn2/impl/Bpmn2JsonMarshaller.java
@@ -1205,6 +1205,7 @@ public class Bpmn2JsonMarshaller {
         
         // data inputs
         DataInput groupDataInput = null;
+        DataInput skippableDataInput = null;
         if(task.getIoSpecification() != null) {
             List<InputSet> inputSetList = task.getIoSpecification().getInputSets();
             StringBuilder dataInBuffer = new StringBuilder();
@@ -1218,6 +1219,9 @@ public class Bpmn2JsonMarshaller {
                     }
                     if(dataIn.getName() != null && dataIn.getName().equals("GroupId")) {
                     	groupDataInput = dataIn;
+                    }
+                    if(dataIn.getName() != null && dataIn.getName().equals("Skippable")) {
+                    	skippableDataInput = dataIn;
                     }
                 }
             }
@@ -1297,6 +1301,11 @@ public class Bpmn2JsonMarshaller {
             				((FormalExpression) datain.getAssignment().get(0).getTo()).getBody() != null &&
             						((FormalExpression) datain.getAssignment().get(0).getTo()).getBody().equals(groupDataInput.getId())) {
             			properties.put("groupid", ((FormalExpression) datain.getAssignment().get(0).getFrom()).getBody());
+            		}
+            		if (skippableDataInput != null && datain.getAssignment().get(0).getTo() != null &&
+            				((FormalExpression) datain.getAssignment().get(0).getTo()).getBody() != null &&
+            						((FormalExpression) datain.getAssignment().get(0).getTo()).getBody().equals(skippableDataInput.getId())) {
+            			properties.put("skippable", ((FormalExpression) datain.getAssignment().get(0).getFrom()).getBody());
             		}
             	}
             } 
