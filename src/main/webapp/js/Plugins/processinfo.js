8a6b38d2f1c44b9bfa7fb7e395543182ae8fba19
Tihomir Surdilovic <tsurdilo@redhat.com>
2012-04-16 10:50:26 -0400

    externalize process info into separate plugin

---
+++ src/main/webapp/js/Plugins/processinfo.js
@@ -0,0 +1,36 @@
+if (!ORYX.Plugins) 
+    ORYX.Plugins = {};
+
+if (!ORYX.Config)
+	ORYX.Config = {};
+
+ORYX.Plugins.ProcessInfo = Clazz.extend({
+	construct: function(facade){
+		this.facade = facade;
+		
+		this.facade.offer({
+			'name':ORYX.I18N.View.showInfo,
+			'functionality': this.showInfo.bind(this),
+			'group': ORYX.I18N.View.infogroup,
+			'icon': ORYX.PATH + "images/information.png",
+			'description': ORYX.I18N.View.showInfoDesc,
+			'index': 1,
+			'minShape': 0,
+			'maxShape': 0,
+			'isEnabled': function(){
+				profileParamName = "profile";
+				profileParamName = profileParamName.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+				regexSa = "[\\?&]"+profileParamName+"=([^&#]*)";
+		        regexa = new RegExp( regexSa );
+		        profileParams = regexa.exec( window.location.href );
+		        profileParamValue = profileParams[1]; 
+				return profileParamValue == "jbpm";
+			}.bind(this)
+		});
+	},
+	showInfo : function() {
+		window.alert("jBPM Designer Version: " + ORYX.VERSION);
+	}
+});
+//window.frames[6].ORYX.EDITOR._canvas.nodes[3]._stencil.icon()
+
