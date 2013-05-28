3dbcb0a622a0f4d1972a8a586c0d298858849413
Alexandre Porcelli <alexandre.porcelli@gmail.com>
2013-05-28 17:41:32 -0300

    fixing standalone designer - basically a issue related to startup ordering of types

--- jbpm-designer-standalone/src/main/java/org/jbpm/designer/uberfire/backend/server/impl/AppSetup.java
+++ jbpm-designer-standalone/src/main/java/org/jbpm/designer/uberfire/backend/server/impl/AppSetup.java
@@ -3,19 +3,15 @@ package org.jbpm.designer.uberfire.backend.server.impl;
 import java.util.HashMap;
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
-import javax.inject.Named;
 
-import org.kie.commons.cluster.ClusterServiceFactory;
-import org.kie.commons.io.IOService;
-import org.kie.commons.io.impl.IOServiceDotFileImpl;
-import org.kie.commons.io.impl.cluster.IOServiceClusterImpl;
-import org.kie.commons.java.nio.file.FileSystem;
+import org.kie.commons.services.cdi.Startup;
+import org.kie.commons.services.cdi.StartupType;
 import org.uberfire.backend.repositories.Repository;
 import org.uberfire.backend.repositories.RepositoryService;
 
 @ApplicationScoped
+@Startup(StartupType.BOOTSTRAP)
 public class AppSetup {
 
     private static final String JBPM_REPO_PLAYGROUND = "jbpm-playground";
@@ -27,54 +23,12 @@ public class AppSetup {
     private final String userName = "guvnorngtestuser1";
     private final String password = "test1234";
 
-    private IOService ioService;
-
-    @Produces
-    @Named("ioStrategy")
-    public IOService ioService() {
-        return ioService;
-    }
-
-    @Inject
-    @Named("clusterServiceFactory")
-    private ClusterServiceFactory clusterServiceFactory;
-
     @Inject
     private RepositoryService repositoryService;
-
-    private FileSystem fs = null;
 
     @PostConstruct
     public void onStartup() {
         try {
-//            Properties repositoryProps = new Properties();
-//            repositoryProps.load(this.getClass().getResourceAsStream("/repository.properties"));
-//
-//
-//            final String originUrl = repositoryProps.getProperty("repository.origin");
-//            final String userName = repositoryProps.getProperty("repository.username");
-//            final String password = repositoryProps.getProperty("repository.password");
-//            final String location = repositoryProps.getProperty("repository.location");
-//            Repository repository = repositoryService.getRepository(location);
-//            if(repository == null) {
-//
-//                repositoryService.cloneRepository("git", location, originUrl, userName, password);
-//                repository = repositoryService.getRepository(location);
-//            }
-//            try {
-//                fs = ioService.newFileSystem(URI.create(repository.getUri()), repository.getEnvironment());
-//
-//            } catch (FileSystemAlreadyExistsException e) {
-//                fs = ioService.getFileSystem(URI.create(repository.getUri()));
-//
-//            }
-
-            if ( clusterServiceFactory == null ) {
-                ioService = new IOServiceDotFileImpl();
-            } else {
-                ioService = new IOServiceClusterImpl( new IOServiceDotFileImpl(), clusterServiceFactory );
-            }
-
             Repository jbpmRepo = repositoryService.getRepository( JBPM_REPO_PLAYGROUND );
             if ( jbpmRepo == null ) {
                 final String userName = "guvnorngtestuser1";
--- jbpm-designer-standalone/src/main/java/org/jbpm/designer/uberfire/backend/server/impl/ApplicationScopedProvider.java
+++ jbpm-designer-standalone/src/main/java/org/jbpm/designer/uberfire/backend/server/impl/ApplicationScopedProvider.java
@@ -1,10 +1,16 @@
 package org.jbpm.designer.uberfire.backend.server.impl;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
 import javax.inject.Named;
 
-import org.uberfire.backend.server.repositories.SystemRepository;
+import org.kie.commons.cluster.ClusterServiceFactory;
+import org.kie.commons.io.IOService;
+import org.kie.commons.io.impl.IOServiceDotFileImpl;
+import org.kie.commons.io.impl.cluster.IOServiceClusterImpl;
+import org.uberfire.backend.repositories.Repository;
 
 import static org.uberfire.backend.server.repositories.SystemRepository.*;
 
@@ -15,9 +21,30 @@ import static org.uberfire.backend.server.repositories.SystemRepository.*;
 @ApplicationScoped
 public class ApplicationScopedProvider {
 
+    @Inject
+    @Named("clusterServiceFactory")
+    private ClusterServiceFactory clusterServiceFactory;
+
+    private IOService ioService;
+
+    @PostConstruct
+    public void setup() {
+        if ( clusterServiceFactory == null ) {
+            ioService = new IOServiceDotFileImpl();
+        } else {
+            ioService = new IOServiceClusterImpl( new IOServiceDotFileImpl(), clusterServiceFactory );
+        }
+    }
+
+    @Produces
+    @Named("ioStrategy")
+    public IOService ioService() {
+        return ioService;
+    }
+
     @Produces
     @Named("system")
-    public SystemRepository systemRepository() {
+    public Repository systemRepository() {
         return SYSTEM_REPO;
     }
 }
