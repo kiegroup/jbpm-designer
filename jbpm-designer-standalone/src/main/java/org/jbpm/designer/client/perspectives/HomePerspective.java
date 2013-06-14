4a119771f0f9d711c4f1072ffcb956c2c34edd10
Tihomir Surdilovic <tsurdilo@redhat.com>
2013-06-14 12:27:48 -0400

    updates to home perspective

--- jbpm-designer-standalone/src/main/java/org/jbpm/designer/client/perspectives/HomePerspective.java
+++ jbpm-designer-standalone/src/main/java/org/jbpm/designer/client/perspectives/HomePerspective.java
@@ -4,13 +4,21 @@ import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
+import com.google.gwt.event.logical.shared.CloseEvent;
+import com.google.gwt.event.logical.shared.CloseHandler;
+import com.google.gwt.user.client.ui.PopupPanel;
+import org.jboss.errai.ioc.client.container.IOCBeanManager;
 import org.kie.workbench.common.widgets.client.handlers.NewResourcePresenter;
 import org.kie.workbench.common.widgets.client.handlers.NewResourcesMenu;
 import org.uberfire.client.annotations.Perspective;
 import org.uberfire.client.annotations.WorkbenchMenu;
 import org.uberfire.client.annotations.WorkbenchPerspective;
+import org.uberfire.client.annotations.WorkbenchToolBar;
+import org.uberfire.client.editors.repository.clone.CloneRepositoryForm;
+import org.uberfire.client.editors.repository.create.CreateRepositoryForm;
 import org.uberfire.client.mvp.PlaceManager;
 import org.uberfire.mvp.Command;
+import org.uberfire.mvp.PlaceRequest;
 import org.uberfire.mvp.impl.DefaultPlaceRequest;
 import org.uberfire.workbench.model.PanelDefinition;
 import org.uberfire.workbench.model.PerspectiveDefinition;
@@ -20,25 +28,47 @@ import org.uberfire.workbench.model.impl.PartDefinitionImpl;
 import org.uberfire.workbench.model.impl.PerspectiveDefinitionImpl;
 import org.uberfire.workbench.model.menu.MenuFactory;
 import org.uberfire.workbench.model.menu.Menus;
+import org.uberfire.workbench.model.toolbar.IconType;
+import org.uberfire.workbench.model.toolbar.ToolBar;
+import org.uberfire.workbench.model.toolbar.impl.DefaultToolBar;
+import org.uberfire.workbench.model.toolbar.impl.DefaultToolBarItem;
+
+import static org.uberfire.workbench.model.toolbar.IconType.DOWNLOAD_ALT;
+import static org.uberfire.workbench.model.toolbar.IconType.FOLDER_CLOSE_ALT;
 
 @ApplicationScoped
 @WorkbenchPerspective(identifier = "home", isDefault = true)
 public class HomePerspective {
+
+    private static String[] PERMISSIONS_ADMIN = new String[]{ "ADMIN" };
+    public static final String RECENT_EDITED_ID = "recentEdited";
+    public static final String RECENT_VIEWED_ID = "recentViewed";
 
     @Inject
     private NewResourcePresenter newResourcePresenter;
 
     @Inject
     private NewResourcesMenu newResourcesMenu;
+
     @Inject
     private PlaceManager placeManager;
+
+    @Inject
+    private IOCBeanManager iocManager;
+
     private PerspectiveDefinition perspective;
     private Menus menus;
+    private ToolBar toolBar;
+
+    private Command newRepoCommand   = null;
+    private Command cloneRepoCommand = null;
 
     @PostConstruct
     public void init() {
+        buildCommands();
         buildPerspective();
         buildMenuBar();
+        buildToolBar();
     }
 
     @Perspective
@@ -50,10 +80,13 @@ public class HomePerspective {
         perspective = new PerspectiveDefinitionImpl();
         perspective.setName( "Home" );
 
+        this.perspective.getRoot().addPart( new PartDefinitionImpl( new DefaultPlaceRequest( "RepositoriesEditor" ) ) );
+
+
         final PanelDefinition west = new PanelDefinitionImpl();
         west.setWidth( 300 );
         west.setMinWidth( 200 );
-        west.addPart( new PartDefinitionImpl( new DefaultPlaceRequest( "org.kie.guvnor.explorer" ) ) );
+        west.addPart( new PartDefinitionImpl( new DefaultPlaceRequest( "FileExplorer" ) ) );
 
         perspective.getRoot().appendChild( Position.WEST, west );
 
@@ -65,18 +98,105 @@ public class HomePerspective {
         return this.menus;
     }
 
+    @WorkbenchToolBar
+    public ToolBar getToolBar() {
+        return this.toolBar;
+    }
+
+    private void buildCommands() {
+        this.cloneRepoCommand = new Command() {
+
+            @Override
+            public void execute() {
+                final CloneRepositoryForm cloneRepositoryWizard = iocManager.lookupBean( CloneRepositoryForm.class ).getInstance();
+                //When pop-up is closed destroy bean to avoid memory leak
+                cloneRepositoryWizard.addCloseHandler( new CloseHandler<PopupPanel>() {
+
+                    @Override
+                    public void onClose( CloseEvent<PopupPanel> event ) {
+                        iocManager.destroyBean( cloneRepositoryWizard );
+                    }
+
+                } );
+                cloneRepositoryWizard.show();
+            }
+
+        };
+
+        this.newRepoCommand = new Command() {
+            @Override
+            public void execute() {
+                final CreateRepositoryForm newRepositoryWizard = iocManager.lookupBean( CreateRepositoryForm.class ).getInstance();
+                //When pop-up is closed destroy bean to avoid memory leak
+                newRepositoryWizard.addCloseHandler( new CloseHandler<CreateRepositoryForm>() {
+                    @Override
+                    public void onClose( CloseEvent<CreateRepositoryForm> event ) {
+                        iocManager.destroyBean( newRepositoryWizard );
+                    }
+                } );
+                newRepositoryWizard.show();
+            }
+        };
+    }
+
     private void buildMenuBar() {
         this.menus = MenuFactory
-                .newTopLevelMenu( "Projects" )
-                .respondsWith( new Command() {
-                    @Override
-                    public void execute() {
-                        placeManager.goTo( "org.kie.guvnor.explorer" );
-                    }
-                } )
-                .endMenu()
+//                .newTopLevelMenu( "Projects" )
+//                .respondsWith( new Command() {
+//                    @Override
+//                    public void execute() {
+//                        placeManager.goTo( "org.kie.guvnor.explorer" );
+//                    }
+//                } )
+//                .endMenu()
                 .newTopLevelMenu( "New" )
                 .withItems( newResourcesMenu.getMenuItems() )
+                .endMenu()
+                .newTopLevelMenu( "Repositories" )
+                .menus()
+                .menu( "Clone Repo" )
+                .withRoles(PERMISSIONS_ADMIN)
+                .respondsWith(cloneRepoCommand)
+                .endMenu()
+                .menu("New Repo")
+                .withRoles(PERMISSIONS_ADMIN)
+                .respondsWith(newRepoCommand)
+                .endMenu()
+                .menu("Recently Edited")
+                .respondsWith(new Command() {
+                    @Override
+                    public void execute() {
+                        PlaceRequest p = new DefaultPlaceRequest("Inbox");
+                        p.addParameter("inboxname", HomePerspective.RECENT_EDITED_ID);
+                        placeManager.goTo(p);
+                    }
+                })
+                .endMenu()
+                .menu("Recently Opened")
+                .respondsWith(new Command() {
+                    @Override
+                    public void execute() {
+                        PlaceRequest p = new DefaultPlaceRequest("Inbox");
+                        p.addParameter("inboxname", HomePerspective.RECENT_VIEWED_ID);
+                        placeManager.goTo(p);
+                    }
+                })
+                .endMenu()
+                .endMenus()
                 .endMenu().build();
+    }
+
+    private void buildToolBar() {
+        this.toolBar = new DefaultToolBar( "file.explorer" );
+        final DefaultToolBarItem i1 = new DefaultToolBarItem( FOLDER_CLOSE_ALT,
+                "New Repository",
+                newRepoCommand );
+        final DefaultToolBarItem i2 = new DefaultToolBarItem( DOWNLOAD_ALT,
+                "Clone Repository",
+                cloneRepoCommand );
+        i1.setRoles( PERMISSIONS_ADMIN );
+        i2.setRoles( PERMISSIONS_ADMIN );
+        toolBar.addItem( i1 );
+        toolBar.addItem( i2 );
     }
 }
