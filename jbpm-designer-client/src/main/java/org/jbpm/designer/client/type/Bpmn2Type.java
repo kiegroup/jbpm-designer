ae411a1ad7a9c2c1d8f00c42e1a84b0c6ff73484
Tihomir Surdilovic <tsurdilo@redhat.com>
2013-12-13 18:52:31 -0500

    BZ 1040121 - Update icons used in Project Explorer - bpmn2

--- jbpm-designer-client/src/main/java/org/jbpm/designer/client/type/Bpmn2Type.java
+++ jbpm-designer-client/src/main/java/org/jbpm/designer/client/type/Bpmn2Type.java
@@ -3,16 +3,20 @@ package org.jbpm.designer.client.type;
 import javax.enterprise.context.ApplicationScoped;
 
 import com.google.gwt.user.client.ui.IsWidget;
+import org.jbpm.designer.client.resources.DesignerEditorResources;
 import org.jbpm.designer.type.Bpmn2TypeDefinition;
 import org.uberfire.client.workbench.type.ClientResourceType;
+import com.google.gwt.user.client.ui.Image;
 
 @ApplicationScoped
 public class Bpmn2Type
         extends Bpmn2TypeDefinition
         implements ClientResourceType {
 
+    private static final Image IMAGE = new Image(DesignerEditorResources.INSTANCE.images().typeForm());
+
     @Override
     public IsWidget getIcon() {
-        return null;
+        return IMAGE;
     }
 }
