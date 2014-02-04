48763bda32d170450f7ed55280555351e504eaec
nmirasch <neus.miras@gmail.com>
2014-02-04 10:03:29 +0100

    BZ:1052858 - i18n is incomplete, code changes needed
    (cherry picked from commit 89cde21b76c9d71bd0a8651509d6952daef3b739)

--- jbpm-designer-client/src/main/java/org/jbpm/designer/client/resources/i18n/DesignerEditorConstants.java
+++ jbpm-designer-client/src/main/java/org/jbpm/designer/client/resources/i18n/DesignerEditorConstants.java
@@ -3,13 +3,6 @@ package org.jbpm.designer.client.resources.i18n;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.i18n.client.Messages;
 
-/**
- * Created with IntelliJ IDEA.
- * User: nmirasch
- * Date: 1/28/14
- * Time: 2:54 PM
- * To change this template use File | Settings | File Templates.
- */
 
 public interface DesignerEditorConstants extends
         Messages {
