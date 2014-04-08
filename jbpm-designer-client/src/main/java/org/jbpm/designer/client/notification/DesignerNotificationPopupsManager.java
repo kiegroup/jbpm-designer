efb61e3c29b9a5aad2667f4407f40221b2de322b
Tihomir Surdilovic <tsurdilo@redhat.com>
2014-04-08 18:12:12 -0400

    BZ 1083547 - Simulation engine: Label for time unit is incorrect [minutes]

---
+++ jbpm-designer-client/src/main/java/org/jbpm/designer/client/notification/DesignerNotificationPopupsManager.java
@@ -0,0 +1,11 @@
+package org.jbpm.designer.client.notification;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: tihomirsurdilovic
+ * Date: 4/8/14
+ * Time: 10:21 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class DesignerNotificationPopupsManager {
+}
