410b8f2a5fee70678150e3f13f3bc36ce67d866a
Tihomir Surdilovic <tsurdilo@redhat.com>
2014-05-02 10:29:44 -0400

    BZ 1093598 - RuleFlow set: Unable to continue working on process after the user adds any task to canvas

--- jbpm-designer-client/src/main/resources/org/jbpm/designer/public/js/itemdeleter.js
+++ jbpm-designer-client/src/main/resources/org/jbpm/designer/public/js/itemdeleter.js
@@ -10,16 +10,31 @@ Extensive.grid.ItemDeleter = Ext.extend(Ext.grid.RowSelectionModel, {
     menuDisabled: true,
     fixed: true,
     id: 'deleter',
-    
-    initEvents: function(){
+    dtype: "",
+
+    setDType: function(dtype) {
+        if(dtype && dtype.length > 0) {
+            this.dtype = dtype;
+        }
+    },
+
+    initEvents: function() {
         Extensive.grid.ItemDeleter.superclass.initEvents.call(this);
         this.grid.on('cellclick', function(grid, rowIndex, columnIndex, e){
 			if(columnIndex==grid.getColumnModel().getIndexById('deleter')) {
 				var record = grid.getStore().getAt(rowIndex);
+
+                ORYX.EDITOR._pluginFacade.raiseEvent({
+                    type 		: ORYX.CONFIG.EVENT_DEF_DELETED,
+                    dtype       : this.dtype,
+                    rcd         : record
+
+                });
+
 				grid.getStore().remove(record);
 				grid.getView().refresh();
 			}
-        });
+        }.bind(this));
     },
     
     renderer: function(v, p, record, rowIndex){
