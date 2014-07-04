908274d3451c06aef2ad8be44b177001c4ac3156
Michael Anstis <manstis@gmail.com>
2014-07-04 10:18:46 +0100

    GUVNOR-2105: WebSphere: Indexers: Circular injection of IOService <--> Indexer.

--- jbpm-designer-standalone/src/main/java/org/jbpm/designer/uberfire/backend/server/impl/ApplicationScopedProvider.java
+++ jbpm-designer-standalone/src/main/java/org/jbpm/designer/uberfire/backend/server/impl/ApplicationScopedProvider.java
@@ -7,21 +7,24 @@ import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.guvnor.common.services.backend.metadata.attribute.OtherMetaView;
+import org.kie.uberfire.metadata.backend.lucene.LuceneConfig;
+import org.kie.uberfire.metadata.io.IOSearchIndex;
+import org.kie.uberfire.metadata.io.IOServiceIndexedImpl;
 import org.uberfire.backend.server.IOWatchServiceNonDotImpl;
 import org.uberfire.commons.cluster.ClusterServiceFactory;
+import org.uberfire.commons.services.cdi.Startup;
+import org.uberfire.commons.services.cdi.StartupType;
 import org.uberfire.io.IOSearchService;
 import org.uberfire.io.IOService;
 import org.uberfire.io.attribute.DublinCoreView;
 import org.uberfire.io.impl.cluster.IOServiceClusterImpl;
 import org.uberfire.java.nio.base.version.VersionAttributeView;
-import org.kie.uberfire.metadata.backend.lucene.LuceneConfig;
-import org.kie.uberfire.metadata.io.IOSearchIndex;
-import org.kie.uberfire.metadata.io.IOServiceIndexedImpl;
 
 /**
  * This class should contain all ApplicationScoped producers
  * required by the application.
  */
+@Startup(StartupType.BOOTSTRAP)
 @ApplicationScoped
 public class ApplicationScopedProvider {
 
@@ -43,21 +46,20 @@ public class ApplicationScopedProvider {
     public void setup() {
 
         final IOService service = new IOServiceIndexedImpl( watchService,
-                                config.getIndexEngine(),
-                                config.getIndexers(),
-                                DublinCoreView.class,
-                                VersionAttributeView.class,
-                                OtherMetaView.class );
-
+                                                            config.getIndexEngine(),
+                                                            DublinCoreView.class,
+                                                            VersionAttributeView.class,
+                                                            OtherMetaView.class );
 
         if ( clusterServiceFactory == null ) {
             ioService = service;
         } else {
             ioService = new IOServiceClusterImpl( service,
-                                        clusterServiceFactory,
-                                        false );
+                                                  clusterServiceFactory,
+                                                  false );
         }
-        this.ioSearchService = new IOSearchIndex(config.getSearchIndex(), ioService);
+        this.ioSearchService = new IOSearchIndex( config.getSearchIndex(),
+                                                  ioService );
     }
 
     @Produces
--- jbpm-designer-standalone/src/main/java/org/jbpm/designer/uberfire/backend/server/impl/LuceneConfigProducer.java
+++ jbpm-designer-standalone/src/main/java/org/jbpm/designer/uberfire/backend/server/impl/LuceneConfigProducer.java
@@ -1,38 +1,31 @@
 package org.jbpm.designer.uberfire.backend.server.impl;
 
-import org.apache.lucene.analysis.Analyzer;
-import org.kie.workbench.common.services.refactoring.backend.server.indexing.RuleAttributeNameAnalyzer;
-import org.kie.workbench.common.services.refactoring.model.index.terms.RuleIndexTerm;
-import org.kie.uberfire.metadata.backend.lucene.LuceneConfig;
-import org.kie.uberfire.metadata.backend.lucene.LuceneConfigBuilder;
-import org.kie.uberfire.metadata.engine.Indexer;
-
+import java.util.HashMap;
+import java.util.Map;
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.inject.Any;
-import javax.enterprise.inject.Instance;
 import javax.enterprise.inject.Produces;
-import javax.inject.Inject;
 import javax.inject.Named;
-import java.util.*;
 
-import static org.apache.lucene.util.Version.LUCENE_40;
+import org.apache.lucene.analysis.Analyzer;
+import org.kie.uberfire.metadata.backend.lucene.LuceneConfig;
+import org.kie.uberfire.metadata.backend.lucene.LuceneConfigBuilder;
+import org.kie.uberfire.metadata.backend.lucene.analyzer.FilenameAnalyzer;
+import org.kie.workbench.common.services.refactoring.backend.server.indexing.RuleAttributeNameAnalyzer;
+import org.kie.workbench.common.services.refactoring.model.index.terms.ProjectRootPathIndexTerm;
+import org.kie.workbench.common.services.refactoring.model.index.terms.RuleIndexTerm;
+
+import static org.apache.lucene.util.Version.*;
 
 @ApplicationScoped
 public class LuceneConfigProducer {
-
-    @Inject
-    @Any
-    private Instance<Indexer> indexers;
 
     private LuceneConfig config;
 
     @PostConstruct
     public void setup() {
-        final Set<Indexer> indexers = getIndexers();
         final Map<String, Analyzer> analyzers = getAnalyzers();
         this.config = new LuceneConfigBuilder().withInMemoryMetaModelStore()
-                .usingIndexers( indexers )
                 .usingAnalyzers( analyzers )
                 .useDirectoryBasedIndex()
                 .useNIODirectory()
@@ -45,21 +38,12 @@ public class LuceneConfigProducer {
         return this.config;
     }
 
-    private Set<Indexer> getIndexers() {
-        if ( indexers == null ) {
-            return Collections.emptySet();
-        }
-        final Set<Indexer> result = new HashSet<Indexer>();
-        for ( Indexer indexer : indexers ) {
-            result.add( indexer );
-        }
-        return result;
-    }
-
     private Map<String, Analyzer> getAnalyzers() {
         return new HashMap<String, Analyzer>() {{
             put( RuleIndexTerm.TERM,
-                    new RuleAttributeNameAnalyzer( LUCENE_40 ) );
+                 new RuleAttributeNameAnalyzer( LUCENE_40 ) );
+            put( ProjectRootPathIndexTerm.TERM,
+                 new FilenameAnalyzer( LUCENE_40 ) );
         }};
     }
 
