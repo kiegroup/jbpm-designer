264b1e0f75da7eede1da177ecb5fa184e69e215b
Toni Rikkola <toni@rikkola.net>
2014-07-29 12:34:43 +0300

    BPMSPL-44 : Improve metadata tab

--- jbpm-designer-client/src/main/java/org/jbpm/designer/client/DesignerPresenter.java
+++ jbpm-designer-client/src/main/java/org/jbpm/designer/client/DesignerPresenter.java
@@ -263,12 +263,10 @@ public class DesignerPresenter {
                                                                  CommonConstants.INSTANCE.MetadataTabTitle() ) {
                                         @Override
                                         public void onFocus() {
-                                            if ( !metadataWidget.isAlreadyLoaded() ) {
                                                 metadataWidget.showBusyIndicator( CommonConstants.INSTANCE.Loading() );
                                                 metadataService.call( new MetadataSuccessCallback( metadataWidget,
                                                                                                    isReadOnly ),
                                                                       new HasBusyIndicatorDefaultErrorCallback( metadataWidget ) ).getMetadata( path );
-                                            }
                                         }
 
                                         @Override
