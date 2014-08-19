6d04adc979889282facd691599b3192bac3c99c8
Tihomir Surdilovic <tsurdilo@redhat.com>
2014-08-19 12:34:45 -0400

    default display on errors

--- jbpm-designer-backend/src/main/java/org/jbpm/designer/web/repository/impl/UUIDBasedJbpmRepository.java
+++ jbpm-designer-backend/src/main/java/org/jbpm/designer/web/repository/impl/UUIDBasedJbpmRepository.java
@@ -1,10 +1,13 @@
 package org.jbpm.designer.web.repository.impl;
 
+import javax.enterprise.event.Event;
+import javax.inject.Inject;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 
 import bpsim.impl.BpsimPackageImpl;
+import org.jbpm.designer.server.service.DefaultDesignerAssetService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.jboss.drools.impl.DroolsPackageImpl;
@@ -27,17 +30,29 @@ public class UUIDBasedJbpmRepository implements IUUIDBasedRepository {
 
     public byte[] load(HttpServletRequest req, String uuid, IDiagramProfile profile, ServletContext servletContext) throws Exception {
         Repository repository = profile.getRepository();
-        String processjson = "";
         String preProcessingParam = req.getParameter("pp");
         String processxml = ((Asset<String>)repository.loadAsset(uuid)).getAssetContent();
 
         if(processxml != null && processxml.length() > 0) {
-        	DroolsPackageImpl.init();
-            BpsimPackageImpl.init();
-            processjson = profile.createUnmarshaller().parseModel(processxml, profile, preProcessingParam);
-
-            return processjson.getBytes("UTF-8");
+        	try {
+                DroolsPackageImpl.init();
+                BpsimPackageImpl.init();
+                String processjson = profile.createUnmarshaller().parseModel(processxml, profile, preProcessingParam);
+                return processjson.getBytes("UTF-8");
+            } catch(Exception e) {
+                return loadDefaultProcess(profile, preProcessingParam);
+            }
         } else {
+            return loadDefaultProcess(profile, preProcessingParam);
+        }
+    }
+
+    private byte[] loadDefaultProcess(IDiagramProfile profile, String preProcessingParam) {
+        try {
+            String defaultProcessContent = DefaultDesignerAssetService.PROCESS_STUB.replaceAll( "\\$\\{processid\\}", "defaultprocessid" );
+            String processjson = profile.createUnmarshaller().parseModel(defaultProcessContent, profile, preProcessingParam);
+            return processjson.getBytes("UTF-8");
+        } catch(Exception e) {
             return new byte[0];
         }
     }
