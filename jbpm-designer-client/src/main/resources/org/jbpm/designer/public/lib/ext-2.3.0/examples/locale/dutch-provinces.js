6b16e3f9c9f3daa703fc8127b89e700a5e05639f
Mohammad Saadati <sadatimoghadam@gmail.com>
2014-08-25 01:59:12 +0430

    Upgrade to Ext JS 2.3.0 was very simple but develop any change at 2.0.2 because it has any document was very hard.
    Now you can see:
    http://dev.sencha.com/deploy/ext-2.3.0/docs/
    http://dev.sencha.com/deploy/yui-ext/examples/samples.html
    and all in lib.
    Thanks.

---
+++ jbpm-designer-client/src/main/resources/org/jbpm/designer/public/lib/ext-2.3.0/examples/locale/dutch-provinces.js
@@ -0,0 +1,25 @@
+/*
+ * Ext JS Library 2.3.0
+ * Copyright(c) 2006-2009, Ext JS, LLC.
+ * licensing@extjs.com
+ * 
+ * http://extjs.com/license
+ */
+
+// some data used in the examples
+Ext.namespace('Ext.exampledata');
+
+Ext.exampledata.dutch_provinces = [
+	['Drenthe'],
+	['Flevoland'],
+	['Friesland'],
+	['Gelderland'],
+	['Groningen'],
+	['Limburg'],
+	['Noord-Brabant'],
+	['Noord-Holland'],
+	['Overijsel'],
+	['Utrecht'],
+	['Zeeland'],
+	['Zuid-Holland']
+    ];
