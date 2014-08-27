23991e7585226f85ac742aa4e112225a9826099b
Michael Anstis <manstis@gmail.com>
2014-08-27 17:35:54 +0100

    GUVNOR-2117: Add 'Package Selector' to 'New Resource' popup

--- jbpm-designer-client/src/main/java/org/jbpm/designer/client/handlers/NewProcessHandler.java
+++ jbpm-designer-client/src/main/java/org/jbpm/designer/client/handlers/NewProcessHandler.java
@@ -11,13 +11,10 @@ import org.jbpm.designer.client.resources.i18n.DesignerEditorConstants;
 import org.jbpm.designer.client.type.Bpmn2Type;
 import org.jbpm.designer.service.DesignerAssetService;
 import org.kie.uberfire.client.callbacks.DefaultErrorCallback;
-import org.kie.workbench.common.services.shared.validation.ValidatorWithReasonCallback;
 import org.kie.workbench.common.widgets.client.handlers.DefaultNewResourceHandler;
 import org.kie.workbench.common.widgets.client.handlers.NewResourcePresenter;
-import org.kie.workbench.common.widgets.client.resources.i18n.CommonConstants;
 import org.uberfire.backend.vfs.Path;
 import org.uberfire.client.mvp.PlaceManager;
-import org.uberfire.client.workbench.widgets.common.ErrorPopup;
 import org.uberfire.mvp.PlaceRequest;
 import org.uberfire.mvp.impl.PathPlaceRequest;
 import org.uberfire.workbench.type.ResourceTypeDefinition;
@@ -63,30 +60,6 @@ public class NewProcessHandler extends DefaultNewResourceHandler {
             }
         }, new DefaultErrorCallback() ).createProcess( pkg.getPackageMainResourcesPath(), buildFileName( baseFileName,
                                                                                                          resourceType ) );
-    }
-
-    @Override
-    public void validate( final String baseFileName,
-                          final ValidatorWithReasonCallback callback ) {
-        if ( pathLabel.getPath() == null ) {
-            ErrorPopup.showMessage( CommonConstants.INSTANCE.MissingPath() );
-            callback.onFailure();
-            return;
-        }
-
-        final String fileName = buildFileName( baseFileName,
-                                               getResourceType() );
-
-        validationService.call( new RemoteCallback<Boolean>() {
-            @Override
-            public void callback( final Boolean response ) {
-                if ( Boolean.TRUE.equals( response ) ) {
-                    callback.onSuccess();
-                } else {
-                    callback.onFailure( CommonConstants.INSTANCE.InvalidFileName0( baseFileName ) );
-                }
-            }
-        } ).isFileNameValid( fileName );
     }
 
 }
