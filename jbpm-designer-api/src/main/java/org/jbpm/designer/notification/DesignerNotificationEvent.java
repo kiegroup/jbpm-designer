0a4d07fbb7ca5fca3999a36f14b4975a22fd46df
Alexandre Porcelli <alexandre.porcelli@gmail.com>
2014-09-04 18:49:27 -0300

    DesignerNotificationEvent is not a core UberFire event, so it doesn't need to extend/implement it

--- jbpm-designer-api/src/main/java/org/jbpm/designer/notification/DesignerNotificationEvent.java
+++ jbpm-designer-api/src/main/java/org/jbpm/designer/notification/DesignerNotificationEvent.java
@@ -2,24 +2,25 @@ package org.jbpm.designer.notification;
 
 import org.jboss.errai.common.client.api.annotations.Portable;
 import org.uberfire.workbench.events.NotificationEvent;
-import org.uberfire.workbench.events.UberFireEvent;
 
 @Portable
-public class DesignerNotificationEvent extends UberFireEvent {
+public class DesignerNotificationEvent {
 
     private final String notification;
     private final NotificationEvent.NotificationType type;
 
     public DesignerNotificationEvent() {
-        this("Designernotification", "message");
-    }
-    public DesignerNotificationEvent( final String notification, final String message ) {
-        this( notification,
-                NotificationEvent.NotificationType.DEFAULT );
+        this( "Designernotification", "message" );
     }
 
     public DesignerNotificationEvent( final String notification,
-                              final NotificationEvent.NotificationType type ) {
+                                      final String message ) {
+        this( notification,
+              NotificationEvent.NotificationType.DEFAULT );
+    }
+
+    public DesignerNotificationEvent( final String notification,
+                                      final NotificationEvent.NotificationType type ) {
         this.notification = notification;
         this.type = type;
     }
