5ccb1a06b7dc4f655e8bd2206c9e33e4e3f8ec15
Igor Golovlyev <  Golovlyev.Igor@cg.ru>
2014-10-02 09:17:49 +0400

    WEBBPMNEXT-422

--- jbpm-designer-backend/src/main/java/org/jbpm/designer/web/server/LoadBpmn2Servlet.java
+++ jbpm-designer-backend/src/main/java/org/jbpm/designer/web/server/LoadBpmn2Servlet.java
@@ -13,15 +13,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.io.FileUtils;
-import org.eclipse.bpmn2.Definitions;
+import org.eclipse.bpmn2.*;
 import org.eclipse.bpmn2.Process;
-import org.eclipse.bpmn2.RootElement;
 import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.impl.EAttributeImpl;
-import org.eclipse.emf.ecore.impl.EStructuralFeatureImpl;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.eclipse.emf.ecore.util.ExtendedMetaData;
 import org.eclipse.emf.ecore.util.FeatureMap;
 import org.jbpm.designer.bpmn2.resource.JBPMBpmn2ResourceFactoryImpl;
 import org.jbpm.designer.bpmn2.resource.JBPMBpmn2ResourceImpl;
@@ -45,49 +41,43 @@ public class LoadBpmn2Servlet extends HttpServlet {
       String procDefPath = req.getParameter("procDefPath");
       String profileParam = req.getParameter("profile");
       String pp = req.getParameter("pp");
-      String uuid = req.getParameter("uuid");
+
+      String convertServiceTasks = "true";//TODO
 
 
       File file = new File(procDefPath);
       String bpmnXmlStr = FileUtils.readFileToString(file);
 
-      IDiagramProfile profile = _profileService.findProfile(req, profileParam);
-      // fix package name if needed
-      String packageName = null;
-      if (uuid != null && !uuid.isEmpty()) {
-        String[] packageAssetName = ServletUtil.findPackageAndAssetInfo(uuid, profile);
-        packageName = packageAssetName[0];
+      if (convertServiceTasks != null && convertServiceTasks.equals("true")) {
+        bpmnXmlStr = bpmnXmlStr.replaceAll("drools:taskName=\".*?\"", "drools:taskName=\"ReadOnlyService\"");
+        bpmnXmlStr = bpmnXmlStr.replaceAll("tns:taskName=\".*?\"", "tns:taskName=\"ReadOnlyService\"");
       }
+
+      IDiagramProfile profile = _profileService.findProfile(req, profileParam);
       Definitions def = ((JbpmProfileImpl) profile).getDefinitions(bpmnXmlStr);
       if (def != null) {
-        List<RootElement> rootElements = def.getRootElements();
-        for (RootElement root : rootElements) {
-          if (root instanceof org.eclipse.bpmn2.Process) {
-            Process process = (Process) root;
-            Iterator<FeatureMap.Entry> iter = process.getAnyAttribute().iterator();
-            FeatureMap.Entry toDeleteFeature = null;
-            while (iter.hasNext()) {
-              FeatureMap.Entry entry = iter.next();
-              if ("packageName".equals(entry.getEStructuralFeature().getName())) {
-                String pname = (String) entry.getValue();
-                if (pname == null || !pname.equals(packageName)) {
-                  toDeleteFeature = entry;
-                }
-              }
-            }
-            if (toDeleteFeature != null) {
-              process.getAnyAttribute().remove(toDeleteFeature);
-              ExtendedMetaData metadata = ExtendedMetaData.INSTANCE;
-              EAttributeImpl extensionAttribute = (EAttributeImpl) metadata.demandFeature("http://www.jboss.org/drools", "packageName", false, false);
-              EStructuralFeatureImpl.SimpleFeatureMapEntry extensionEntry = new EStructuralFeatureImpl.SimpleFeatureMapEntry(extensionAttribute, packageName);
-              process.getAnyAttribute().add(extensionEntry);
+        def.setTargetNamespace("http://www.omg.org/bpmn20");
+
+        if (convertServiceTasks != null && convertServiceTasks.equals("true")) {
+          // fix the data input associations for converted tasks
+          List<RootElement> rootElements = def.getRootElements();
+          for (RootElement root : rootElements) {
+            if (root instanceof Process) {
+              updateTaskDataInputs((Process) root, def);
             }
           }
         }
+
+
         // get the xml from Definitions
         ResourceSet rSet = new ResourceSetImpl();
         rSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("bpmn2", new JBPMBpmn2ResourceFactoryImpl());
         JBPMBpmn2ResourceImpl bpmn2resource = (JBPMBpmn2ResourceImpl) rSet.createResource(URI.createURI("virtual.bpmn2"));
+        bpmn2resource.getDefaultLoadOptions().put(JBPMBpmn2ResourceImpl.OPTION_ENCODING, "UTF-8");
+        bpmn2resource.getDefaultLoadOptions().put(JBPMBpmn2ResourceImpl.OPTION_DEFER_IDREF_RESOLUTION, true);
+        bpmn2resource.getDefaultLoadOptions().put(JBPMBpmn2ResourceImpl.OPTION_DISABLE_NOTIFY, true);
+        bpmn2resource.getDefaultLoadOptions().put(JBPMBpmn2ResourceImpl.OPTION_PROCESS_DANGLING_HREF, JBPMBpmn2ResourceImpl.OPTION_PROCESS_DANGLING_HREF_RECORD);
+        bpmn2resource.setEncoding("UTF-8");
         rSet.getResources().add(bpmn2resource);
         bpmn2resource.getContents().add(def);
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
@@ -100,6 +90,39 @@ public class LoadBpmn2Servlet extends HttpServlet {
     }
     catch (Exception ex) {
       throw new ServletException();
+    }
+  }
+
+  private void updateTaskDataInputs(FlowElementsContainer container, Definitions def) {
+    List<FlowElement> flowElements = container.getFlowElements();
+    for (FlowElement fe : flowElements) {
+      if (fe instanceof Task && !(fe instanceof UserTask)) {
+        Task task = (Task) fe;
+        boolean foundReadOnlyServiceTask = false;
+        Iterator<FeatureMap.Entry> iter = task.getAnyAttribute().iterator();
+        while (iter.hasNext()) {
+          FeatureMap.Entry entry = iter.next();
+          if (entry.getEStructuralFeature().getName().equals("taskName")) {
+            if (entry.getValue().equals("ReadOnlyService")) {
+              foundReadOnlyServiceTask = true;
+            }
+          }
+        }
+
+        if (foundReadOnlyServiceTask) {
+          if (task.getDataInputAssociations() != null) {
+            List<DataInputAssociation> dataInputAssociations = task.getDataInputAssociations();
+            for (DataInputAssociation dia : dataInputAssociations) {
+              if (dia.getTargetRef().getId().endsWith("TaskNameInput")) {
+                ((FormalExpression) dia.getAssignment().get(0).getFrom()).setBody("ReadOnlyService");
+              }
+            }
+          }
+        }
+      }
+      else if (fe instanceof FlowElementsContainer) {
+        updateTaskDataInputs((FlowElementsContainer) fe, def);
+      }
     }
   }
 }
