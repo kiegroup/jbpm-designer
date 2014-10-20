254e34f9d473a2995c2a77cc0579dd517cd60eea
Tihomir Surdilovic <tsurdilo@redhat.com>
2014-10-20 12:05:20 -0400

    fix for standalone menubar setup

--- jbpm-designer-standalone/src/main/java/org/jbpm/designer/client/perspectives/HomePerspective.java
+++ jbpm-designer-standalone/src/main/java/org/jbpm/designer/client/perspectives/HomePerspective.java
@@ -1,10 +1,12 @@
 package org.jbpm.designer.client.perspectives;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
 import org.jboss.errai.ioc.client.container.SyncBeanManager;
 import org.kie.workbench.common.screens.projecteditor.client.menu.ProjectMenu;
+import org.kie.workbench.common.services.security.KieWorkbenchACL;
 import org.kie.workbench.common.widgets.client.handlers.NewResourcePresenter;
 import org.kie.workbench.common.widgets.client.handlers.NewResourcesMenu;
 import org.uberfire.client.annotations.Perspective;
@@ -43,41 +45,62 @@ public class HomePerspective {
     @Inject
     private ProjectMenu projectMenu;
 
-    @Perspective
-    public PerspectiveDefinition buildPerspective() {
+    @Inject
+    private KieWorkbenchACL kieACL;
 
-        final PerspectiveDefinitionImpl perspective = new PerspectiveDefinitionImpl( MultiListWorkbenchPanelPresenter.class.getName() );
-        perspective.setName( "Author" );
+    private PerspectiveDefinition perspective;
+    private Menus menus;
+
+    @PostConstruct
+    public void init() {
+        buildPerspective();
+        buildMenuBar();
+    }
+
+    @Perspective
+    public PerspectiveDefinition getPerspective() {
+        return this.perspective;
+    }
+
+    @WorkbenchMenu
+    public Menus getMenus() {
+        return this.menus;
+    }
+
+    public void buildPerspective() {
+
+        this.perspective = new PerspectiveDefinitionImpl( MultiListWorkbenchPanelPresenter.class.getName() );
+        this.perspective.setName( "Administration" );
 
         final PanelDefinition west = new PanelDefinitionImpl( SimpleWorkbenchPanelPresenter.class.getName() );
         west.setWidth( 400 );
         west.setMinWidth( 350 );
         west.addPart( new PartDefinitionImpl( new DefaultPlaceRequest( "org.kie.guvnor.explorer" ) ) );
 
-        perspective.getRoot().insertChild( CompassPosition.WEST,
+        this.perspective.getRoot().insertChild( CompassPosition.WEST,
                                            west );
-
-        return perspective;
     }
 
-    @WorkbenchMenu
-    private Menus buildMenuBar() {
-        return MenuFactory
+    private void buildMenuBar() {
+        this.menus = MenuFactory
                 .newTopLevelMenu( "Projects" )
-                .respondsWith( new Command() {
+                .withRoles(kieACL.getGrantedRoles("wb_administration"))
+                .respondsWith(new Command() {
                     @Override
                     public void execute() {
-                        placeManager.goTo( "org.kie.guvnor.explorer" );
+                        placeManager.goTo("org.kie.guvnor.explorer");
                     }
-                } )
+                })
                 .endMenu()
 
-                .newTopLevelMenu( "New" )
-                .withItems( newResourcesMenu.getMenuItems() )
+                .newTopLevelMenu("New")
+                .withRoles(kieACL.getGrantedRoles("wb_administration"))
+                .withItems(newResourcesMenu.getMenuItems())
                 .endMenu()
 
-                .newTopLevelMenu( "Tools" )
-                .withItems( projectMenu.getMenuItems() )
+                .newTopLevelMenu("Tools")
+                .withRoles(kieACL.getGrantedRoles("wb_administration"))
+                .withItems(projectMenu.getMenuItems())
                 .endMenu()
 
                 .build();
