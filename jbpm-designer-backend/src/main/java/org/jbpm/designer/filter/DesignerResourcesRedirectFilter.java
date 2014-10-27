b98b657fa8455e2e7e36e2cfe50de9b45d038224
Maciej Swiderski <mswiders@redhat.com>
2014-10-27 18:52:20 +0100

    changed response.sendRedirect to requestDispatch.forward for resource redirect filter to rely on server side redirect only

--- jbpm-designer-backend/src/main/java/org/jbpm/designer/filter/DesignerResourcesRedirectFilter.java
+++ jbpm-designer-backend/src/main/java/org/jbpm/designer/filter/DesignerResourcesRedirectFilter.java
@@ -1,7 +1,6 @@
 package org.jbpm.designer.filter;
 
 import java.io.IOException;
-
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -9,7 +8,6 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 public class DesignerResourcesRedirectFilter implements Filter {
 
@@ -24,15 +22,19 @@ public class DesignerResourcesRedirectFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response,
             FilterChain chain) throws IOException, ServletException {
-        
-        if (request instanceof HttpServletRequest) {
-            String requestUrl = ((HttpServletRequest) request).getRequestURI();
-            String contextPath = ((HttpServletRequest) request).getContextPath();
-            String redirect = contextPath + redirectTo + requestUrl.replaceFirst((contextPath+DESIGNER_PREFIX), "");
-            ((HttpServletResponse) response).sendRedirect(redirect);
 
-        } else {
-            chain.doFilter(request, response);
+        try {
+            if (request instanceof HttpServletRequest) {
+                String requestUrl = ((HttpServletRequest) request).getRequestURI();
+                String contextPath = ((HttpServletRequest) request).getContextPath();
+                String redirect = redirectTo + requestUrl.replaceFirst((contextPath+DESIGNER_PREFIX), "");
+                request.getRequestDispatcher(redirect).forward(request, response);
+
+            } else {
+                chain.doFilter(request, response);
+            }
+        } catch (Throwable e) {
+            e.printStackTrace();
         }
     }
 
