e57bd26715cdecd88a99fc7690dc0364fed2c184
Tihomir Surdilovic <tsurdilo@redhat.com>
2014-11-11 13:11:13 -0500

    BZ 1162680 - Data Assignments Editor doesn't load all data assignments where DataInput and DataOutput contains same variable names
    Conflicts:
    
    	jbpm-designer-client/src/main/resources/org/jbpm/designer/public/js/Plugins/propertywindow.js

--- jbpm-designer-client/src/main/resources/org/jbpm/designer/public/js/Plugins/propertywindow.js
+++ jbpm-designer-client/src/main/resources/org/jbpm/designer/public/js/Plugins/propertywindow.js
@@ -3311,8 +3311,8 @@ Ext.form.ComplexDataAssignmenField = Ext.extend(Ext.form.TriggerField,  {
                         }
                     }
                     if( dataInputsOnlyVals.indexOf(fromPart) >= 0 && variableDefsOnlyVals.indexOf(innerParts[1]) >= 0 ){
-                        // if its also a vardef - pass
-                        if(variableDefsOnlyVals.indexOf(fromPart) < 0) {
+                        // if its also a data output - pass
+                        if(dataOutputsOnlyVals.indexOf(fromPart) < 0) {
                             hasErrors = true;
                         }
                     }
