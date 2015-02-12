0abaa7af1e9762df37cafa15784f0a7f5cb420fa
Tihomir Surdilovic <tsurdilo@redhat.com>
2015-02-11 21:57:49 -0500

    fix for 3+ deep subprocess di flowelements

--- jbpm-designer-backend/src/main/java/org/jbpm/designer/bpmn2/impl/Bpmn2JsonMarshaller.java
+++ jbpm-designer-backend/src/main/java/org/jbpm/designer/bpmn2/impl/Bpmn2JsonMarshaller.java
@@ -2662,7 +2662,7 @@ public class Bpmn2JsonMarshaller {
 	    Bounds bounds = ((BPMNShape) findDiagramElement(plane, subProcess)).getBounds();
 	    for (FlowElement flowElement: subProcess.getFlowElements()) {
             if(coordianteManipulation) {
-                marshallFlowElement(flowElement, plane, generator, bounds.getX(), bounds.getY(), preProcessingData, def);
+                marshallFlowElement(flowElement, plane, generator, bounds.getX() + xOffset, bounds.getY() + yOffset, preProcessingData, def);
             } else {
                 marshallFlowElement(flowElement, plane, generator, 0, 0, preProcessingData, def);
             }
