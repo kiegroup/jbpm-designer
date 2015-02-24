2c6c15ce8f2e1cea59bfe9309f40476d92150dc7
Jeremy Lindop <jlindop@redhat.com>
2015-02-24 16:31:08 +0000

    BZ1130465: IE9 - designer does not load fully in IE9

---
+++ jbpm-designer-client/src/main/resources/org/jbpm/designer/public/js/ie-utils.js
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2015 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// BZ1130465 - Designer doesn't fully load in IE9 due to missing function Range.createContextualFragment
+if ((typeof Range !== "undefined") && !Range.prototype.createContextualFragment)
+{
+    Range.prototype.createContextualFragment = function(html)
+    {
+        var frag = document.createDocumentFragment(),
+                div = document.createElement("div");
+        frag.appendChild(div);
+        div.outerHTML = html;
+        return frag;
+    };
+}
