e7cd34d98fc6d3b94d03c070b744d51ec60cbf5b
Tihomir Surdilovic <tsurdilo@redhat.com>
2015-07-16 16:45:32 -0400

    Attempt to rename file affects the wrong BPM process in BPM Suite 6.1.0

--- jbpm-designer-client/src/main/java/org/jbpm/designer/client/DesignerPresenter.java
+++ jbpm-designer-client/src/main/java/org/jbpm/designer/client/DesignerPresenter.java
@@ -204,7 +204,7 @@ public class DesignerPresenter
                                                            public void execute( final FileNameAndCommitMessage details ) {
                                                                baseView.showLoading();
                                                                copyService.call( getCopySuccessCallback(),
-                                                                                 new HasBusyIndicatorDefaultErrorCallback( baseView ) ).copy( mypath,
+                                                                                 new HasBusyIndicatorDefaultErrorCallback( baseView ) ).copy( versionRecordManager.getPathToLatest(),
                                                                                                                                               details.getNewFileName(),
                                                                                                                                               details.getCommitMessage() );
                                                            }
@@ -225,7 +225,7 @@ public class DesignerPresenter
                                                                public void execute( final FileNameAndCommitMessage details ) {
                                                                    baseView.showLoading();
                                                                    renameService.call( getRenameSuccessCallback(),
-                                                                                       new HasBusyIndicatorDefaultErrorCallback( baseView ) ).rename( mypath,
+                                                                                       new HasBusyIndicatorDefaultErrorCallback( baseView ) ).rename( versionRecordManager.getPathToLatest(),
                                                                                                                                                       details.getNewFileName(),
                                                                                                                                                       details.getCommitMessage() );
                                                                }
