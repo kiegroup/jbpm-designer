a29fab5887e01f3c4e3f35eeaca9546f9c40838b
Paulo Martins <paulovmr@gmail.com>
2016-05-20 10:56:41 -0300

    GUVNOR-2502: Consolidate client-side bootstrap logic in various workbench implementations (#256)

--- jbpm-designer-standalone/src/main/java/org/jbpm/designer/client/StandaloneEntryPoint.java
+++ jbpm-designer-standalone/src/main/java/org/jbpm/designer/client/StandaloneEntryPoint.java
@@ -17,120 +17,37 @@ package org.jbpm.designer.client;
 
 import javax.inject.Inject;
 
-import com.google.gwt.animation.client.Animation;
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.dom.client.Element;
-import com.google.gwt.dom.client.Style;
-import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.ui.RootPanel;
-import org.jboss.errai.bus.client.api.BusErrorCallback;
-import org.jboss.errai.bus.client.api.messaging.Message;
-import org.jboss.errai.common.client.api.Caller;
-import org.jboss.errai.common.client.api.RemoteCallback;
-import org.jboss.errai.ioc.client.api.AfterInitialization;
-import org.jboss.errai.ioc.client.api.EntryPoint;
-import org.jboss.errai.ioc.client.container.SyncBeanManager;
-import org.jboss.errai.security.shared.service.AuthenticationService;
-import org.jbpm.designer.client.resources.StandaloneResources;
+import org.guvnor.common.services.shared.config.AppConfigService;
 import org.guvnor.common.services.shared.security.KieWorkbenchACL;
-import org.guvnor.common.services.shared.security.KieWorkbenchPolicy;
+import org.jboss.errai.common.client.api.Caller;
+import org.jboss.errai.ioc.client.api.EntryPoint;
+import org.jbpm.designer.client.resources.StandaloneResources;
 import org.kie.workbench.common.services.shared.security.KieWorkbenchSecurityService;
-import org.uberfire.client.mvp.ActivityManager;
-import org.uberfire.client.mvp.PlaceManager;
-import org.uberfire.client.workbench.widgets.menu.WorkbenchMenuBarPresenter;
-import org.uberfire.mvp.Command;
-import org.uberfire.workbench.model.menu.MenuFactory;
+import org.kie.workbench.common.services.shared.service.PlaceManagerActivityService;
+import org.kie.workbench.common.workbench.client.entrypoint.DefaultWorkbenchEntryPoint;
+import org.kie.workbench.common.workbench.client.menu.DefaultWorkbenchFeaturesMenusHelper;
+import org.uberfire.client.mvp.ActivityBeansCache;
 
-/**
- * GWT's Entry-point for jBPM Designer
- */
 @EntryPoint
-public class StandaloneEntryPoint {
+public class StandaloneEntryPoint extends DefaultWorkbenchEntryPoint {
+
+    protected DefaultWorkbenchFeaturesMenusHelper menusHelper;
 
     @Inject
-    private SyncBeanManager manager;
+    public StandaloneEntryPoint( final Caller<AppConfigService> appConfigService,
+                                 final Caller<KieWorkbenchSecurityService> kieSecurityService,
+                                 final Caller<PlaceManagerActivityService> pmas,
+                                 final KieWorkbenchACL kieACL,
+                                 final ActivityBeansCache activityBeansCache,
+                                 final DefaultWorkbenchFeaturesMenusHelper menusHelper ) {
+        super( appConfigService, kieSecurityService, pmas, kieACL, activityBeansCache );
+        this.menusHelper = menusHelper;
 
-    @Inject
-    private WorkbenchMenuBarPresenter menubar;
-
-    @Inject
-    private PlaceManager placeManager;
-
-    @Inject
-    private ActivityManager activityManager;
-
-    @Inject
-    private KieWorkbenchACL kieACL;
-
-    @Inject
-    private Caller<KieWorkbenchSecurityService> kieSecurityService;
-
-    @Inject
-    private Caller<AuthenticationService> authService;
-
-    @AfterInitialization
-    public void startApp() {
-        kieSecurityService.call( new RemoteCallback<String>() {
-            public void callback( final String str ) {
-                KieWorkbenchPolicy policy = new KieWorkbenchPolicy( str );
-                kieACL.activatePolicy( policy );
-                loadStyles();
-                setupMenu();
-                hideLoadingPopup();
-            }
-        } ).loadPolicy();
+        addCustomSecurityLoadedCallback( policy -> StandaloneResources.INSTANCE.CSS().ensureInjected() );
     }
 
-    private void loadStyles() {
-        StandaloneResources.INSTANCE.CSS().ensureInjected();
+    @Override
+    protected void setupMenu() {
+        menusHelper.addLogoutMenuItem();
     }
-
-    private void setupMenu() {
-        menubar.addMenus(
-                MenuFactory.newTopLevelMenu( "Logout" ).respondsWith( new Command() {
-                    @Override
-                    public void execute() {
-                        logout();
-                    }
-                } ).endMenu().build() );
-    }
-
-    private void logout() {
-        authService.call( new RemoteCallback<Void>() {
-            @Override
-            public void callback( Void response ) {
-                redirect( GWT.getHostPageBaseURL() + "login.jsp" );
-            }
-        }, new BusErrorCallback() {
-            @Override
-            public boolean error( Message message,
-                                  Throwable throwable ) {
-                Window.alert( "Logout failed: " + throwable );
-                return true;
-            }
-        } ).logout();
-    }
-
-    //Fade out the "Loading application" pop-up
-    private void hideLoadingPopup() {
-        final Element e = RootPanel.get( "loading" ).getElement();
-
-        new Animation() {
-
-            @Override
-            protected void onUpdate( double progress ) {
-                e.getStyle().setOpacity( 1.0 - progress );
-            }
-
-            @Override
-            protected void onComplete() {
-                e.getStyle().setVisibility( Style.Visibility.HIDDEN );
-            }
-        }.run( 500 );
-    }
-
-    public static native void redirect( String url )/*-{
-        $wnd.location = url;
-    }-*/;
-
 }
