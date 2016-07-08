295b73a9ce7731c72a550556ce51d30919582ef8
Jozef Marko <jomarko@redhat.com>
2016-07-08 09:16:27 +0200

    RHBPMS-1913: default input and output set for MI subprocess

--- jbpm-designer-backend/src/test/java/org/jbpm/designer/bpmn2/impl/Bpmn2UnmarshallingTest.java
+++ jbpm-designer-backend/src/test/java/org/jbpm/designer/bpmn2/impl/Bpmn2UnmarshallingTest.java
@@ -1068,6 +1068,45 @@ public class Bpmn2UnmarshallingTest {
         verifyAttribute(dataOutput, "dtype", "Integer");
     }
 
+    @Test
+    public void testTaskInputOutputSet() throws Exception {
+        Process process = getRootProcess(loader.loadProcessFromJson("taskInputOutputSet.json"));
+        assertTrue(process.getFlowElements().get(1) instanceof UserTask);
+        UserTask task = (UserTask) process.getFlowElements().get(1);
+        assertEquals("userTask", task.getName());
+        InputSet inputSet = task.getIoSpecification().getInputSets().get(0);
+        OutputSet outputSet = task.getIoSpecification().getOutputSets().get(0);
+        assertEquals(4, inputSet.getDataInputRefs().size());
+        assertEquals("firstInput", inputSet.getDataInputRefs().get(0).getName());
+        assertEquals("secondInput", inputSet.getDataInputRefs().get(1).getName());
+        assertEquals("TaskName", inputSet.getDataInputRefs().get(2).getName());
+        assertEquals("Skippable", inputSet.getDataInputRefs().get(3).getName());
+        assertEquals(1, outputSet.getDataOutputRefs().size());
+        assertEquals("firstOutput", outputSet.getDataOutputRefs().get(0).getName());
+    }
+
+    @Test
+    public void testSubprocessDefaultInputOutputSets() throws Exception {
+        Process process = getRootProcess(loader.loadProcessFromJson("defaultSubprocessInputOutputSets.json"));
+        assertTrue(process.getFlowElements().get(1) instanceof SubProcess);
+        SubProcess subProcess = (SubProcess) process.getFlowElements().get(1);
+        InputSet inputSet = subProcess.getIoSpecification().getInputSets().get(0);
+        OutputSet outputSet = subProcess.getIoSpecification().getOutputSets().get(0);
+        assertEquals(0, inputSet.getDataInputRefs().size());
+        assertEquals(0, outputSet.getDataOutputRefs().size());
+    }
+
+    @Test
+    public void testSubprocessDefaultOutputSet() throws Exception {
+        Process process = getRootProcess(loader.loadProcessFromJson("subprocessDefaultOutputSet.json"));
+        assertTrue(process.getFlowElements().get(1) instanceof SubProcess);
+        SubProcess subProcess = (SubProcess) process.getFlowElements().get(1);
+        InputSet inputSet = subProcess.getIoSpecification().getInputSets().get(0);
+        OutputSet outputSet = subProcess.getIoSpecification().getOutputSets().get(0);
+        assertEquals(1, inputSet.getDataInputRefs().size());
+        assertEquals(0, outputSet.getDataOutputRefs().size());
+    }
+
     private FlowElement getFlowElement(List<FlowElement> elements, String name) {
         for(FlowElement element : elements) {
             if (element.getName() != null && name.compareTo(element.getName()) == 0) {
