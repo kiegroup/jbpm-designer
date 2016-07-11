d818ab471a839a8344997eb6a0f89ad12e15a2af
Jeremy Lindop <jlindop@redhat.com>
2016-07-11 14:29:56 +0100

    JBPM-5179: i18n - Fix generation of task form via "Edit Task Form" button for task with non-Latin chars (#344)

--- jbpm-designer-backend/src/test/java/org/jbpm/designer/web/server/TaskFormsEditorServletTest.java
+++ jbpm-designer-backend/src/test/java/org/jbpm/designer/web/server/TaskFormsEditorServletTest.java
@@ -15,6 +15,18 @@
 
 package org.jbpm.designer.web.server;
 
+import java.io.File;
+import java.net.URL;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.io.FileUtils;
+import org.eclipse.bpmn2.Definitions;
+import org.eclipse.bpmn2.Process;
+import org.eclipse.bpmn2.RootElement;
 import org.jbpm.designer.helper.TestHttpServletRequest;
 import org.jbpm.designer.helper.TestHttpServletResponse;
 import org.jbpm.designer.helper.TestServletConfig;
@@ -23,23 +35,17 @@ import org.jbpm.designer.repository.Asset;
 import org.jbpm.designer.repository.AssetBuilderFactory;
 import org.jbpm.designer.repository.Repository;
 import org.jbpm.designer.repository.RepositoryBaseTest;
+import org.jbpm.designer.repository.UriUtils;
+import org.jbpm.designer.repository.VFSFileSystemProducer;
 import org.jbpm.designer.repository.filters.FilterByExtension;
 import org.jbpm.designer.repository.impl.AssetBuilder;
-import org.jbpm.designer.repository.VFSFileSystemProducer;
 import org.jbpm.designer.repository.vfs.VFSRepository;
 import org.jbpm.designer.web.profile.impl.JbpmProfileImpl;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.File;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 public class TaskFormsEditorServletTest  extends RepositoryBaseTest {
 
@@ -79,7 +85,7 @@ public class TaskFormsEditorServletTest  extends RepositoryBaseTest {
         params.put("uuid", uniqueId);
         params.put("action", "save");
         params.put("profile", "jbpm");
-        params.put("taskname", "evaluate");
+        params.put("taskname", Base64.encodeBase64String(UriUtils.encode("evaluate").getBytes()));
         params.put("tfvalue", "this is simple task content");
         params.put("formtype", "ftl");
 
@@ -129,7 +135,7 @@ public class TaskFormsEditorServletTest  extends RepositoryBaseTest {
         params.put("uuid", uniqueId);
         params.put("action", "load");
         params.put("profile", "jbpm");
-        params.put("taskname", "evaluate");
+        params.put("taskname", Base64.encodeBase64String(UriUtils.encode("evaluate").getBytes()));
         params.put("tfvalue", "this is simple task content");
         params.put("formtype", "ftl");
 
@@ -143,5 +149,44 @@ public class TaskFormsEditorServletTest  extends RepositoryBaseTest {
         String formData =      new String(response.getContent());
         System.out.println(formData);
         assertEquals("this is simple task content", formData);
+    }
+
+
+    @Test
+    public void testLoadForm_i18nName() throws Exception {
+
+        Repository repository = new VFSRepository(producer.getIoService());
+        ((VFSRepository)repository).setDescriptor(descriptor);
+        profile.setRepository(repository);
+        AssetBuilder builder = AssetBuilderFactory.getAssetBuilder(Asset.AssetType.Text);
+        builder.content("bpmn2 content")
+                .type("bpmn2")
+                .name("BPTaskForm_i18nNames")
+                .location("/defaultPackage");
+        String uniqueId = repository.createAsset(builder.getAsset());
+
+        // setup parameters
+        String taskName = "проверить";
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("uuid", uniqueId);
+        params.put("action", "load");
+        params.put("profile", "jbpm");
+        params.put("taskname", Base64.encodeBase64String(UriUtils.encode(taskName).getBytes()));
+        params.put("tfvalue", "this is simple task content");
+        params.put("formtype", "ftl");
+
+        TaskFormsEditorServlet taskFormsEditorServlet = new TaskFormsEditorServlet();
+        taskFormsEditorServlet.setProfile(profile);
+
+        taskFormsEditorServlet.init(new TestServletConfig(new TestServletContext(repository)));
+        TestHttpServletResponse response = new TestHttpServletResponse();
+        taskFormsEditorServlet.doPost(new TestHttpServletRequest(params), response);
+
+        Collection<Asset> forms = repository.listAssets("/defaultPackage", new FilterByExtension("ftl"));
+        assertNotNull(forms);
+        assertEquals(1, forms.size());
+        Iterator<Asset> assets = forms.iterator();
+        Asset asset1 = assets.next();
+        assertEquals(taskName + "-taskform", asset1.getName());
     }
 }
