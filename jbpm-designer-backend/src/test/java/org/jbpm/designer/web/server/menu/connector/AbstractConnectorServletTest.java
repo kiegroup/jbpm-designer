4b88298ab873aa0ac44b831064d2b616de8e6c02
Jozef Marko <jomarko@redhat.com>
2016-07-13 16:13:20 +0200

    JBPM-5181: added missing license headers (#346)

--- jbpm-designer-backend/src/test/java/org/jbpm/designer/web/server/menu/connector/AbstractConnectorServletTest.java
+++ jbpm-designer-backend/src/test/java/org/jbpm/designer/web/server/menu/connector/AbstractConnectorServletTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2016 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
 package org.jbpm.designer.web.server.menu.connector;
 
 import static org.junit.Assert.assertEquals;
@@ -14,7 +29,6 @@ import org.jbpm.designer.repository.RepositoryBaseTest;
 import org.jbpm.designer.web.profile.IDiagramProfile;
 import org.jbpm.designer.web.profile.IDiagramProfileService;
 
-import org.jbpm.designer.web.server.menu.MenuConnectorServlet;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
