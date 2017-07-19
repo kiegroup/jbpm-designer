a3d76780488676597326f069e41355ffef425ac2
Paulo Martins <paulovmr@gmail.com>
2017-07-19 16:33:13 -0300

    GUVNOR-3150: [Library] Disabling different user role permissions does not work (#636)

--- jbpm-designer-client/src/test/java/org/jbpm/designer/client/DesignerPresenterTest.java
+++ jbpm-designer-client/src/test/java/org/jbpm/designer/client/DesignerPresenterTest.java
@@ -19,20 +19,46 @@ package org.jbpm.designer.client;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.guvnor.common.services.project.client.security.ProjectController;
+import org.guvnor.common.services.project.context.ProjectContext;
+import org.guvnor.common.services.project.model.Project;
 import org.guvnor.common.services.shared.metadata.model.Overview;
 import org.jbpm.designer.client.parameters.DesignerEditorParametersPublisher;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.kie.workbench.common.widgets.client.menu.FileMenuBuilderImpl;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.uberfire.backend.vfs.Path;
+import org.uberfire.ext.editor.commons.client.history.VersionRecordManager;
+import org.uberfire.ext.editor.commons.client.menu.BasicFileMenuBuilder;
+import org.uberfire.ext.editor.commons.client.validation.DefaultFileNameValidator;
+import org.uberfire.workbench.model.menu.MenuItem;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class DesignerPresenterTest {
+
+    @Mock
+    protected BasicFileMenuBuilder menuBuilder;
+
+    @Mock
+    protected VersionRecordManager versionRecordManager;
+
+    @Spy
+    @InjectMocks
+    protected FileMenuBuilderImpl fileMenuBuilder;
+
+    @Mock
+    protected ProjectController projectController;
+
+    @Mock
+    protected ProjectContext workbenchContext;
 
     @Mock
     private DesignerView view;
@@ -46,13 +72,25 @@ public class DesignerPresenterTest {
     @Spy
     private Map<String, String> parameters = new HashMap<String, String>();
 
-    @InjectMocks
-    @Spy
-    private DesignerPresenter presenter = new DesignerPresenter(view) {
-        @Override
-        protected void resetEditorPages(final Overview overview) {
-        }
-    };
+    private DesignerPresenter presenter;
+
+    @Before
+    public void setup() {
+        presenter = spy(new DesignerPresenter(view) {
+
+            {
+                this.fileMenuBuilder = DesignerPresenterTest.this.fileMenuBuilder;
+                this.projectController = DesignerPresenterTest.this.projectController;
+                this.workbenchContext = DesignerPresenterTest.this.workbenchContext;
+                this.versionRecordManager = DesignerPresenterTest.this.versionRecordManager;
+                this.designerEditorParametersPublisher = DesignerPresenterTest.this.designerEditorParametersPublisher;
+            }
+
+            @Override
+            protected void resetEditorPages(final Overview overview) {
+            }
+        });
+    }
 
     @Test
     public void testSetup() {
@@ -74,5 +112,39 @@ public class DesignerPresenterTest {
         assertEquals(1,
                      parameters.size());
         assertTrue(parameters.containsKey("readonly"));
+    }
+
+    @Test
+    public void testMakeMenuBar() {
+        doReturn(mock(Project.class)).when(workbenchContext).getActiveProject();
+        doReturn(true).when(projectController).canUpdateProject(any());
+
+        presenter.makeMenuBar();
+
+        verify(fileMenuBuilder).addSave(any(MenuItem.class));
+        verify(fileMenuBuilder).addCopy(any(Path.class),
+                                        any(DefaultFileNameValidator.class));
+        verify(fileMenuBuilder).addRename(any(Path.class),
+                                          any(DefaultFileNameValidator.class));
+        verify(fileMenuBuilder).addDelete(any(Path.class));
+    }
+
+    @Test
+    public void testMakeMenuBarWithoutUpdateProjectPermission() {
+        doReturn(mock(Project.class)).when(workbenchContext).getActiveProject();
+        doReturn(false).when(projectController).canUpdateProject(any());
+
+        presenter.makeMenuBar();
+
+        verify(fileMenuBuilder,
+               never()).addSave(any(MenuItem.class));
+        verify(fileMenuBuilder,
+               never()).addCopy(any(Path.class),
+                                any(DefaultFileNameValidator.class));
+        verify(fileMenuBuilder,
+               never()).addRename(any(Path.class),
+                                  any(DefaultFileNameValidator.class));
+        verify(fileMenuBuilder,
+               never()).addDelete(any(Path.class));
     }
 }
