af7197b051a2857792a43a58ce73ed1900cccc64
Guilherme Caponetto <638737+caponetto@users.noreply.github.com>
2020-11-26 08:07:59 -0300

    AF-1799: Fix more tests after mockito upgrade.

--- jbpm-designer-client/src/test/java/org/jbpm/designer/client/shared/AssignmentDataTest.java
+++ jbpm-designer-client/src/test/java/org/jbpm/designer/client/shared/AssignmentDataTest.java
@@ -23,13 +23,13 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(MockitoJUnitRunner.Silent.class)
 public class AssignmentDataTest extends AssignmentBaseTest {
 
     @Before
