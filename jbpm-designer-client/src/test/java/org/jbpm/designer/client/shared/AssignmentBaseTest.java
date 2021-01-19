db520b6a012a323ac350d45acf546410a13fb28c
Tihomir Surdilovic <tsurdilo@redhat.com>
2021-01-18 21:50:32 -0500

    fix AssignmentBaseTest
    Signed-off-by: Tihomir Surdilovic <tsurdilo@redhat.com>

--- jbpm-designer-client/src/test/java/org/jbpm/designer/client/shared/AssignmentBaseTest.java
+++ jbpm-designer-client/src/test/java/org/jbpm/designer/client/shared/AssignmentBaseTest.java
@@ -46,11 +46,11 @@ public class AssignmentBaseTest {
                             designerEditorConstants);
 
         // Mock StringUtils URL Encoding methods
-        Mockito.when(utils.urlDecode(Mockito.any())).thenAnswer(invocation -> {
+        Mockito.when(utils.urlDecode(Mockito.anyString())).thenAnswer(invocation -> {
             Object[] args = invocation.getArguments();
             return urlDecode((String) args[0]);
         });
-        Mockito.when(utils.urlEncode(Mockito.any())).thenAnswer(invocation -> {
+        Mockito.when(utils.urlEncode(Mockito.anyString())).thenAnswer(invocation -> {
             Object[] args = invocation.getArguments();
             return urlEncode((String) args[0]);
         });
